f41a45029d35011f961edac42c0ed47375f796e3
TauMing <tauming.hew@utegration.com>
2015-03-17 12:17:25 -0500

    added lister to top left controller

--- test/tm/nav/view/TopLeft.controller.js
+++ test/tm/nav/view/TopLeft.controller.js
@@ -1,12 +1,18 @@
 /*globals sap, jQuery, tm*/
+/*jslint nomen: true*/
 
 (function () {
     'use strict';
+    
+    jQuery.sap.require('sap.ui.core.routing.HashChanger');
     
     sap.ui.core.mvc.Controller.extend('tm.nav.view.TopLeft');
     
     tm.nav.view.TopLeft.prototype.onInit = function () {
         jQuery.sap.log.info('[Controller tm.nav.view.TopLeft] onInit');
+        
+        sap.ui.core.UIComponent.getRouterFor(this).getRoute('main001').attachPatternMatched(this._routePatternMatched, this);
+        sap.ui.core.UIComponent.getRouterFor(this).getRoute('main004').attachPatternMatched(this._routePatternMatched, this);
     };
     
     tm.nav.view.TopLeft.prototype.onBeforeRendering = function () {
@@ -19,6 +25,18 @@
     
     tm.nav.view.TopLeft.prototype.onExit = function () {
         jQuery.sap.log.info('[Controller tm.nav.view.TopLeft] onExit');
+    };
+    
+    tm.nav.view.TopLeft.prototype.pressed = function (oEvent) {
+        var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+        oRouter.navTo('main004', {
+            bpNum: '123',
+            caNum: '567'
+        });
+    };
+    
+    tm.nav.view.TopLeft.prototype._routePatternMatched = function (oEvent) {
+        console.log(oEvent.getParameters());
     };
     
 }());
