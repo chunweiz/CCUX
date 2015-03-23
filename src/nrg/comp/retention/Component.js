cc8f9aaa636f221d4650064c9a479c2056fea85e
TauMing <tauming.hew@utegration.com>
2015-03-23 12:58:23 -0500

    added base component setup to nrg

---
+++ src/nrg/comp/retention/Component.js
@@ -0,0 +1,67 @@
+/*globals sap, nrg, jQuery*/
+/*jslint nomen: true*/
+
+(function () {
+    'use strict';
+    
+    jQuery.sap.declare('nrg.comp.retention.Component');
+    jQuery.sap.require('nrg.nav.GreedyRouter');
+    
+    sap.ui.core.UIComponent.extend('nrg.comp.retention.Component', {
+        metadata: {
+            dependencies: {
+                libs: ['sap.m', 'sap.ui.layout', 'ute.ui.commons']
+            },
+            
+            rootView: {
+                viewName: 'nrg.view.App',
+				type: sap.ui.core.mvc.ViewType.XML
+            },
+            
+            routing: {
+                config: {
+                    routerClass: nrg.nav.GreedyRouter,
+                    viewType: sap.ui.core.mvc.ViewType.XML,
+                    viewPath : 'nrg.view'
+                },
+                
+                routes: {
+                    main001: {
+                        pattern: '',
+                        view: 'TopLeftDefault',
+                        targetControl: 'TopLeftNav',
+                        targetAggregation: 'content',
+                        clearTarget: true
+                    }
+                }
+            }
+        }
+    });
+    
+    nrg.comp.retention.Component.prototype.init = function () {
+        sap.ui.core.UIComponent.prototype.init.apply(this);
+        
+        var oRoutes = this.getMetadata().getRoutes(),
+            oRouter = this.getRouter(),
+            sName;
+        
+        //Add a callback to each routes
+        for (sName in oRoutes) {
+            if (oRoutes.hasOwnProperty(sName)) {
+                oRoutes[sName].callback = this._routeCallback;
+            }
+        }
+        
+        oRouter.setGreedy(true);
+        oRouter.initialize();
+    };
+    
+    nrg.comp.retention.Component.prototype.destroy = function () {
+        sap.ui.core.UIComponent.prototype.destory.apply(this, arguments);
+    };
+
+    nrg.comp.retention.Component.prototype._routeCallback = function (route, args, config, targetControl, view) {
+        
+    };
+    
+}());
---
+++ src/nrg/view/App.controller.js
@@ -0,0 +1,12 @@
+/*globals sap, jQuery*/
+
+(function () {
+    'use strict';
+    
+    sap.ui.core.mvc.Controller.extend('nrg.view.App', {
+        onInit: function () {
+            alert('test');
+        }
+    });
+    
+}());
