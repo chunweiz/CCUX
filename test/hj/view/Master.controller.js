9cd3e3b425025dda97eb68bb5587c5dc01e0ed4d
hjlin <hung-ju@utegration.com>
2015-04-09 14:38:05 -0500

    Changing for playback

--- test/hj/view/Master.controller.js
+++ test/hj/view/Master.controller.js
@@ -1,9 +1,11 @@
 sap.ui.controller("sap.ui.demo.myFiori.view.Master", {
 
+    /*
 	handleListItemPress : function (evt) {
 		var context = evt.getSource().getBindingContext();
 		this.nav.to("Detail", context);
 	},
+    */
     
     onButtonClick : function(evt) {
         var oDialogFragment = sap.ui.xmlfragment("sap.ui.demo.myFiori.view.MasterFragment",this);  
@@ -24,5 +26,9 @@ sap.ui.controller("sap.ui.demo.myFiori.view.Master", {
 
     testCheckBox: function(evt){
         alert("Checkbox Checked or Unchecked") ;
+    },
+    onGoToTestPage: function(evt){
+        alert("test") ;
+        this.nav.to("Detail");
     }
 });
