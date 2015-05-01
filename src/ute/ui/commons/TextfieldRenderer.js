c8c889bdb436f4153985b5b7955040ce7baf8488
hjlin <hung-ju@utegration.com>
2015-05-01 10:29:34 -0500

    Changing Input to Textfield

--- src/ute/ui/commons/TextfieldRenderer.js
+++ src/ute/ui/commons/TextfieldRenderer.js
@@ -1,41 +1,37 @@
-/*!
- * ${copyright}
- */
+/*global sap*/
 
-// Provides default renderer for control sap.ui.commons.TextField
 sap.ui.define(['jquery.sap.global', 'sap/ui/core/Renderer', 'sap/ui/core/ValueStateSupport'],
 	function (jQuery, Renderer, ValueStateSupport) {
         "use strict";
 
-	    var InputRenderer = {};
+	    var TextfieldRenderer = {};
 
-	/**
-	 * Renders the HTML for the given control, using the provided {@link sap.ui.core.RenderManager}.
-	 *
-	 * @param {sap.ui.core.RenderManager} oRenderManager The RenderManager that can be used for writing to the render output buffer.
-	 * @param {sap.ui.commons.TextField}
-	 *            oTextField The TextField control that should be rendered.
-	 */
-	    InputRenderer.render = function(oRm, oInput) {
-            if( oInput.getFieldType() === "field2" ){
-                if( oInput.getLabel() ){
-                    oRm.write( "<Label"  ) ;
-                    oRm.addClass( "uteUiInputType2Label" ) ;
-                    oRm.writeClasses() ;
-                    oRm.write( ">" ) ;
-                    oRm.write( oInput.getLabel() + ":" ) ;
-                    oRm.write( "</Label>" ) ;
+	    TextfieldRenderer.render = function (oRm, oTextfield) {
+
+            oRm.write('<span');
+            oRm.addClass('uteTextfield');
+            oRm.writeClasses();
+            oRm.write('>');
+
+            if (oTextfield.getFieldType() === "Underlined") {
+                if (oTextfield.getLabel()) {
+                    oRm.write('<label');
+                    oRm.addClass('uteTextfield-underlined-label');
+                    oRm.writeClasses();
+                    oRm.write('>');
+                    oRm.write(oTextfield.getLabel() + ':');
+                    oRm.write('</label>');
                 }
-                oRm.write(  "<input" ) ;
-                oRm.writeControlData(oInput) ;
-                oRm.writeAttribute("id", oInput.getId() ) ;
-                oRm.writeAttribute("name" , oInput.getName() );
-                oRm.writeAttribute("placeholder" , oInput.getPlaceholder() ) ;
-                oRm.addStyle("width", oInput.getWidth() );
-                oRm.addClass( "uteUiInputType2" ) ;
+                oRm.write('<input');
+                oRm.writeControlData(oTextfield);
+                oRm.writeAttribute('id', oTextfield.getId());
+                oRm.writeAttribute('name', oTextfield.getName());
+                oRm.writeAttribute('placeholder', oTextfield.getPlaceholder());
+                oRm.addStyle('width', oTextfield.getWidth());
+                oRm.addClass('uteTextfield-underlined-input') ;
                 oRm.writeStyles();
                 oRm.writeClasses();
-                oRm.write( ">" ) ;
+                oRm.write('>');
             }
             else{       //default situation, so not specified as field type 2 then filed1
                 oRm.write(  "<input" ) ;
@@ -49,10 +45,10 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Renderer', 'sap/ui/core/ValueSt
                 oRm.writeClasses();
                 oRm.write( ">" ) ;
             }
-
+            oRm.write('</span>');
         };
 
 
-	return InputRenderer;
+	return TextfieldRenderer;
 
 }, /* bExport= */ true);
