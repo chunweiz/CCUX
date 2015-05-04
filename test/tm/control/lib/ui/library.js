c9cae193fee4808f72fa512e2c623a031725a235
tmhew <tauming.hew@utegration.com>
2015-05-04 13:22:43 -0500

    test - extend standard control

--- test/tm/control/lib/ui/library.js
+++ test/tm/control/lib/ui/library.js
@@ -1,20 +1,26 @@
 /*globals sap, tm*/
 
-sap.ui.define(['jquery.sap.global', 'sap/ui/core/library'],
+sap.ui.define(
+    [
+        'jquery.sap.global',
+        'sap/ui/core/library'
+    ],
+
 	function (jQuery) {
 		'use strict';
 
 		sap.ui.getCore().initLibrary({
 			name: 'tm.control.lib.ui',
-			version: '1.0.0.0',
+			version: '1.0.0',
 			dependencies: ['sap.ui.core'],
 			interfaces: [],
 			controls: [
-				'tm.control.lib.ui.Division'
+				'tm.control.lib.ui.Division',
+				'tm.control.lib.ui.Button'
 			],
 			elements: []
 		});
 
 		return tm.control.lib.ui;
-		
-	}, /* bExport= */ false);
+	}
+);
