0b63ca3468d72a6b87687b7344d1d71d36757e85
hjlin <hung-ju@utegration.com>
2015-05-05 17:31:55 -0500

    Still in progress

--- src/ute/ui/commons/Table.js
+++ src/ute/ui/commons/Table.js
@@ -43,13 +43,34 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './Row', './library']
             }
         }});
 
+        Table.prototype.getBinding = function (sName) {
+            var oBinding;
+            if (sName === 'rows') {
+                oBinding = sap.ui.core.Element.prototype.getBinding.call(this, sName);
+                return oBinding;
+            }
+        };
+
         Table.prototype._createRows = function () {
             var aCols = this.getColumns(),
                 oTemplate = new Row(this.getId() + '-rows'),
-                oBinding = this.getBinding('rows'), //function not implemented yet
-                oBindingInfo = this.getBindingInfo('rows'); //function not implemented yet
+                oBinding = this.getBinding('rows'),
+                oBindingInfo = this.getBindingInfo('rows'),
+                aContexts = oBinding.getContext(),
+                i,
+                oClone;
 
+            for (i = 0; i < aCols.length; i = i + 1) {
+                if (aContexts && aContexts[i]) {
+                    oClone = oTemplate.clone('row' + i);
+                    oClone.setBindingContext(aContexts[i], oBindingInfo.model);
+                } else {
+                    oClone.setBindingContext(null);
+                }
+                this.addAggregation('rows', oClone, true);
+            }
 
+            oTemplate.destroy();
         };
 
         return Table;
