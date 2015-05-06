5c5712fc9fd02f972311a2ae4a8c238434e4ff7a
unknown <hlin@TXHQHLIN-L1.nrgenergy.com>
2015-05-06 13:30:03 -0500

    Table developing

--- src/ute/ui/commons/TableRenderer.js
+++ src/ute/ui/commons/TableRenderer.js
@@ -8,8 +8,33 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/theming/Parameters'],
         var TableRenderer = {};
 
         TableRenderer.render = function (rm, oTable) {
+            var aCols = oTable.getColumns(),
+                iColumnCount = aCols.length,
+                i;
+
+
             oTable._createRows();
 
+            rm.write('<div><table');
+            rm.addClass('uteTable');
+            rm.writeClasses();
+            rm.write('>');
+
+            //Render column first
+            rm.write('<tr');
+            rm.addClass('uteTable-rowHeader');
+            rm.writeClasses();
+            rm.write('>');
+            for (i = 0; i < iColumnCount; i = i + 1) {
+
+            }
+
+            rm.write('</tr>');
+            //End rendering column
+
+
+
+            rm.write('</table></div>');
 
         };
 
