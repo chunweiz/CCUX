47b2e8eb70ab43ee4ffe6f501a6866b09b2e63c3
tmhew <tauming.hew@utegration.com>
2015-05-07 18:16:23 -0500

    ScrollContainer sample implementation

--- test/tm/control/lib/ui/ScrollContainerRenderer.js
+++ test/tm/control/lib/ui/ScrollContainerRenderer.js
@@ -1,14 +1,60 @@
 /*globals sap*/
 
 sap.ui.define(
-    [
-        'sap/ui/core/Renderer',
-        'sap/m/ScrollContainerRenderer'
-    ],
+    [],
 
-    function (CoreRenderer, ScrollContainerRenderer) {
-        return CoreRenderer.extend(ScrollContainerRenderer);
+    function () {
+        'use strict';
+
+        var Renderer = {};
+
+        Renderer.render = function (oRm, oControl) {
+            oRm.write('<div');
+            oRm.writeControlData(oControl);
+
+            if (oControl.getWidth()) {
+                oRm.addStyle('width', oControl.getWidth());
+            }
+
+            if (oControl.getHeight()) {
+                oRm.addStyle('height', oControl.getHeight());
+            }
+            oRm.writeStyles();
+
+            if (oControl.getVertical()) {
+                if (!oControl.getHorizontal()) {
+                    oRm.addClass('sapMScrollContV');
+                } else {
+                    oRm.addClass('sapMScrollContVH');
+                }
+            } else if (oControl.getHorizontal()) {
+                oRm.addClass('sapMScrollContH');
+            }
+
+            oRm.addClass('sapMScrollCont');
+            oRm.writeClasses();
+
+            if (oControl.getTooltip_AsString()) {
+                oRm.writeAttributeEscaped('title', oControl.getTooltip_AsString());
+            }
+
+            if (oControl.getFocusable()) {
+                oRm.writeAttributeEscaped('tabindex', '0');
+            }
+
+            oRm.write('><div id="' + oControl.getId() + '-scroll" class="sapMScrollContScroll">');
+
+            oControl.getContent().forEach(function (oContent) {
+                oRm.renderControl(oContent);
+            });
+
+            oRm.write('</div></div>');
+        };
+
+        return Renderer;
     },
 
     true
 );
+
+
