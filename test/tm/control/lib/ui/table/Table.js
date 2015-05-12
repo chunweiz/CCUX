e7f11ab59fb735470300d62881937eadfa835af7
tmhew <tauming.hew@utegration.com>
2015-05-12 11:07:53 -0500

    example - table - template - cleanup

--- test/tm/control/lib/ui/table/Table.js
+++ test/tm/control/lib/ui/table/Table.js
@@ -69,37 +69,6 @@ sap.ui.define(
             oTemplate.destroy();
         };
 
-//        Control.prototype.bindRows = function (oBindingInfo) {
-//            return this.bindAggregation('rows', oBindingInfo);
-//        };
-//
-//        Control.prototype.getBinding = function (sName) {
-//            var oBinding;
-//
-//            sName = sName || 'rows';
-//            oBinding = sap.ui.core.Element.prototype.getBinding().call(this, sName);
-//
-//            return oBinding;
-//        };
-//
-//        Control.prototype.updateRows = function (sReason) {
-//
-//        };
-//
-//        Control.prototype._createRows = function () {
-//            var oBinding = this.getBinding('rows');
-//
-//        };
-//
-//        Control.prototype._bindAggregation = function (sName, sPath, oTemplate, oSorter, aFilters) {
-//            var oBinding;
-//
-//            sap.ui.core.Element.prototype._bindAggregation.apply(this, arguments);
-//            oBinding = this.getBinding('rows');
-//
-//            return this;
-//        };
-
         return Control;
     },
 
