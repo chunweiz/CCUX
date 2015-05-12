bee9c3f662c607e456cc8be0851eaeeba0d8bca1
unknown <hlin@TXHQHLIN-L1.nrgenergy.com>
2015-05-12 16:25:35 -0500

    Table style working

--- src/ute/ui/commons/TableColumnRenderer.js
+++ src/ute/ui/commons/TableColumnRenderer.js
@@ -10,17 +10,42 @@ sap.ui.define(['jquery.sap.global'],
         TableColumnRenderer.render = function (oRm, oTableColumn) {
             oRm.write('<tr');
             oRm.writeControlData(oTableColumn);
+            oRm.addStyle('width', oTableColumn.getWidth());
+            oRm.addClass('uteTb-column');
+            switch (oTableColumn.getParent().getTableType()) {
+            case 'InvoiceTable':
+                oRm.addClass('uteTb-column-invoice');
+                break;
+            case 'CheckbookTable':
+                break;
+            case 'DppTable':
+                break;
+            case 'DppDeniedTable':
+                break;
+            case 'CampaignTable':
+                break;
+            }
+            oRm.writeStyles();
+            oRm.writeClasses();
             oRm.write('>');
 
             //Render columns aggregation
             oTableColumn.getCells().forEach(function (oCell) {
-                oRm.write('<th>');
+                oRm.write('<th');
+                oRm.addStyle('width', oCell.getWidth());
+                oRm.writeStyles();
+                oRm.write('>');
+                oCell.setWidth('');
                 oRm.renderControl(oCell);
                 oRm.write('</th>');
             });
 
             oRm.write('</tr>');
+            oRm.write('<div></div>');
         };
+
+
+
 
         return TableColumnRenderer;
 
