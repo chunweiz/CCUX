0c4ef42d39c2d7b1cc285adc5310b1a50c945554
tmhew <tauming.hew@utegration.com>
2015-05-12 11:06:47 -0500

    hj - example - table - template

--- test/tm/control/lib/ui/table/Table.js
+++ test/tm/control/lib/ui/table/Table.js
@@ -40,12 +40,33 @@ sap.ui.define(
 
         Control.prototype.updateRows = function (sReason) {
             if (this.getDomRef()) {
-                return true;
+                CoreControl.prototype.updateRows.apply(this, arguments);
             }
         };
 
         Control.prototype._createRows = function () {
+            var oTemplate, oClone, oBinding, oBindingInfo, aContext;
 
+            oTemplate = new Row();
+
+            this.getColumns().forEach(function (oColumn) {
+                if (oColumn.getTemplate()) {
+                    oClone = oColumn.getTemplate().clone();
+                    oTemplate.addCell(oClone);
+                }
+            });
+
+            this.destroyAggregation('rows', true);
+            oBinding = this.getBinding('rows');
+            oBindingInfo = this.mBindingInfos.rows;
+
+            oBinding.getContexts().forEach(function (oContext) {
+                oClone = oTemplate.clone();
+                oClone.setBindingContext(oContext, oBindingInfo.model);
+                this.addAggregation('rows', oClone, true);
+            }.bind(this));
+
+            oTemplate.destroy();
         };
 
 //        Control.prototype.bindRows = function (oBindingInfo) {
