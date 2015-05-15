4ed6c47223d93902421febebca3200c8ebc00224
tmhew <tauming.hew@utegration.com>
2015-05-15 14:01:13 -0500

    delete old structure

--- test/hj/webserver.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname));
-console.log('Start hosting from ' + __dirname);
-
-app.listen(process.env.port || 3000);
-console.log('Listening to port ' + (process.env.port || 3000));
