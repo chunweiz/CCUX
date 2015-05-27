a3c2ac28ae47a18ffc0acab6b3928f5f6da92dcb
tmhew <tauming.hew@utegration.com>
2015-05-27 00:45:17 -0500

    custom type - CA

--- ZE_CCUX_TEST/test/tm/message/validation/type/ContractAccountNumber.js
+++ ZE_CCUX_TEST/test/tm/message/validation/type/ContractAccountNumber.js
@@ -26,9 +26,13 @@ sap.ui.define(
 
         CustomType.prototype.setFormatOptions = function (oFormatOptions) {
             this.oFormatOptions = oFormatOptions;
+        };
 
-            if ($.isEmptyObject(this.oFormatOptions)) {
-                this.oFormatOptions = {
+        CustomType.prototype.setConstraints = function (oConstraints) {
+            this.oConstraints = oConstraints;
+
+            if ($.isEmptyObject(this.oConstraints)) {
+                this.oConstraints = {
                     padding: false,
                     optional: false
                 };
@@ -38,19 +42,35 @@ sap.ui.define(
                 minIntegerDigits: 1,
                 maxIntegerDigits: 12
             });
-
         };
 
+        // Expected model type
         CustomType.prototype.parseValue = function (oValue, sInternalType) {
+            console.log('parseValue ... ' + oValue);
 
+            var iResult;
+
+            iResult = this._oNumberFormat.parse(oValue);
+
+            if (isNaN(iResult)) {
+                throw new ParseException('Invalid contract account number');
+            }
         };
 
+        // Model value meets constraint requirements
         CustomType.prototype.validateValue = function (oValue) {
+            console.log('validateValue ... ' + oValue);
 
+            if ((oValue === undefined || oValue === null) && !this.oConstraints.optional) {
+                throw new ValidateException('Contract account number cannot be empty');
+            }
         };
 
+        // Model to Output
         CustomType.prototype.formatValue = function (oValue, sInternalType) {
+            console.log('formatValue ... ' + oValue);
 
+            return oValue;
         };
 
         return CustomType;
