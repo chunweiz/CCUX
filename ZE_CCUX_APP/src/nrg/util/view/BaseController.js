bd4d2b83feeacd0b1942e2cfda284df6379638d8
tmhew <tauming.hew@utegration.com>
2015-05-27 23:31:35 -0500

    BaseController implementation

--- ZE_CCUX_APP/src/nrg/util/view/BaseController.js
+++ ZE_CCUX_APP/src/nrg/util/view/BaseController.js
@@ -1,29 +1,110 @@
 /*global sap*/
+/*jslint nomen:true*/
 
 sap.ui.define(
     [
-        'sap/ui/core/mvc/Controller'
+        'jquery.sap.global',
+        'sap/ui/core/mvc/Controller',
+        'sap/ui/core/message/ControlMessageProcessor',
+        'sap/ui/core/message/Message'
     ],
 
-    function (CoreController) {
+    function ($, Controller, ControlMessageProcessor, Message) {
         'use strict';
 
-        var BaseController = CoreController.extend('nrg.util.view.BaseController', {
+        var BaseController = Controller.extend('nrg.util.view.BaseController', {
             constructor: function (sName) {
-                CoreController.prototype.constructor.apply(this, arguments);
+                Controller.prototype.constructor.apply(this, arguments);
             }
         });
 
         BaseController.prototype.getEventBus = function () {
-
+            this.getOwnerComponent().getEventBus();
         };
 
         BaseController.prototype.navTo = function (sName, oParameters, bReplace) {
+            var oRouter, oRouteParams, bHistReplace;
 
+            oRouter = this.getOwnerComponent().getRouter();
+            oRouteParams = oParameters || {};
+            bHistReplace = bReplace || false;
+
+            oRouter.navTo(sName, oRouteParams, bHistReplace);
         };
 
-        BaseController.prototype.addControlMessage = function () {
+        BaseController.prototype._getControlMessageProcessor = function () {
+            if (!this._oControlMessageProcessor) {
+                this._oControlMessageProcessor = new ControlMessageProcessor();
+            }
 
+            return this._oControlMessageProcessor;
+        };
+
+        BaseController.prototype._getMessageManager = function () {
+            if (!this._oMessageManager) {
+                this._oMessageManager = sap.ui.getCore().getMessageManager();
+            }
+
+            return this._oMessageManager;
+        };
+
+        BaseController.prototype.addControlMessage = function (sMessage, sType, oTarget) {
+            var oMessage, sMsgType, sTarget;
+
+            sMsgType = sType || sap.ui.core.MessageType.None;
+
+            if (oTarget instanceof sap.ui.base.Event) {
+                sTarget = [oTarget.getParameter('id'), oTarget.getParameter('property')].join('/');
+            } else {
+                sTarget = oTarget;
+            }
+
+            oMessage = new Message({
+                message: sMessage,
+                type: sMsgType,
+                target: sTarget,
+                processor: this._getControlMessageProcessor()
+            });
+
+            this._getMessageManager().addMessages(oMessage);
+        };
+
+        BaseController.prototype.addMessage = function (sMessage, sType) {
+            var oMessage, sMsgType;
+
+            sMsgType = sType || sap.ui.core.MessageType.None;
+            oMessage = new Message({
+                message: sMessage,
+                type: sMsgType,
+                processor: this._getControlMessageProcessor()
+            });
+
+            this._getMessageManager().addMessages(oMessage);
+        };
+
+        BaseController.prototype.removeAllMessages = function () {
+            this._getMessageManager().removeAllMessages();
+        };
+
+        BaseController.prototype.removeControlMessages = function (oTarget) {
+            var oMessageManager, aMessage, sTarget;
+
+            oMessageManager = this._getMessageManager();
+            aMessage = oMessageManager.getMessageModel().getData();
+
+            if (oTarget instanceof sap.ui.base.Event) {
+                sTarget = [oTarget.getParameter('id'), oTarget.getParameter('property')].join('/');
+            } else {
+                sTarget = oTarget;
+            }
+
+            if (aMessage && !$.isEmptyObject(aMessage)) {
+                aMessage.forEach(function (oMessage) {
+                    if (oMessage.target === sTarget) {
+                        oMessageManager.removeMessages(oMessage);
+                    }
+                }.bind(this));
+            }
         };
 
         return BaseController;
