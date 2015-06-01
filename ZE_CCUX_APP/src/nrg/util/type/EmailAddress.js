e5dc9d093f292c28d1123b1ac002ca58932ef45b
navya <navya.basavaraju@utegration.com>
2015-06-01 10:01:38 -0500

    Email Address type added in src folder

--- ZE_CCUX_APP/src/nrg/util/type/EmailAddress.js
+++ ZE_CCUX_APP/src/nrg/util/type/EmailAddress.js
@@ -1,5 +1,5 @@
 /*global sap*/
-/*jslint nomen:true*/
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 regexp: true */
 
 sap.ui.define(
     [
@@ -23,18 +23,60 @@ sap.ui.define(
             return 'nrg.util.type.EmailAddress';
         };
 
-        CustomType.prototype.formatValue = function (oValue, sInternalType) {
-            throw new FormatException('');
+        CustomType.prototype.setFormatOptions = function (oFormatOptions) {
+            this.oFormatOptions = oFormatOptions;
         };
 
+        CustomType.prototype.setConstraints = function (oConstraints) {
+            this.oConstraints = oConstraints;
+
+            if ($.isEmptyObject(this.oConstraints)) {
+                this.oConstraints = {
+                    mandatory: false
+                };
+            }
+        };
+
+        // Expected model type
         CustomType.prototype.parseValue = function (oValue, sInternalType) {
-            throw new ParseException('');
+            console.log('parseValue ... ' + oValue);
+
+
+            if (oValue === undefined || oValue === null || oValue.trim() === '') {
+                return oValue;
+            }
+
+            var emailRegex = /^[^.\s()\[\],;:@][^\s()\[\],;:@]+[^.\s()\[\],;:@]@[a-zA-Z0-9]+\..+/i;
+            if (!(emailRegex.test(oValue))) {
+                throw new ParseException('Invalid email');
+            }
+
+            return oValue.toLowerCase();
         };
 
+        // Model value meets constraint requirements
         CustomType.prototype.validateValue = function (oValue) {
-            throw new ValidateException('');
+            console.log('validateValue ... [' + oValue + ']');
+
+            if ((oValue === undefined || oValue === null || oValue.trim() === '') && this.oConstraints.mandatory) {
+                throw new ValidateException('Email cannot be empty');
+            }
+
+            if (oValue.length < 1 || oValue.length > 241) {
+                throw new ValidateException('Email Address length exceeds(allowed upto 241 char)');
+            }
+
+            return oValue;
         };
 
+        // Model to Output
+        CustomType.prototype.formatValue = function (oValue, sInternalType) {
+            console.log('formatValue ... ' + oValue);
+
+
+            return oValue;
+
+        };
         return CustomType;
     }
 );
