27f360ce01287bdc478a5bf5a450b45afa971f49
navya <navya.basavaraju@utegration.com>
2015-06-01 10:25:47 -0500

    Driving License Type added

--- ZE_CCUX_APP/src/nrg/util/type/DrivingLicenseNumber.js
+++ ZE_CCUX_APP/src/nrg/util/type/DrivingLicenseNumber.js
@@ -1,5 +1,7 @@
 /*global sap*/
-/*jslint nomen:true*/
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 regexp: true */
+/*global define */
+
 
 sap.ui.define(
     [
@@ -23,16 +25,55 @@ sap.ui.define(
             return 'nrg.util.type.DrivingLicenseNumber';
         };
 
-        CustomType.prototype.formatValue = function (oValue, sInternalType) {
-            throw new FormatException('');
+        CustomType.prototype.setFormatOptions = function (oFormatOptions) {
+            this.oFormatOptions = oFormatOptions;
         };
 
+        CustomType.prototype.setConstraints = function (oConstraints) {
+            this.oConstraints = oConstraints;
+
+            if ($.isEmptyObject(this.oConstraints)) {
+                this.oConstraints = {
+                    mandatory: false
+                };
+            }
+        };
+
+        // Expected model type
         CustomType.prototype.parseValue = function (oValue, sInternalType) {
-            throw new ParseException('');
+            console.log('parseValue ... ' + oValue);
+
+
+            if (oValue === undefined || oValue === null || oValue.trim() === '') {
+                return oValue;
+            }
+
+            var dlRegex = /^.{1,13}$/;
+            if (!(dlRegex.test(oValue))) {
+                throw new ParseException('Invalid Driving License');
+            }
+
+            return oValue.toLowerCase();
         };
 
+        // Model value meets constraint requirements
         CustomType.prototype.validateValue = function (oValue) {
-            throw new ValidateException('');
+            console.log('validateValue ... [' + oValue + ']');
+
+            if ((oValue === undefined || oValue === null || oValue.trim() === '') && this.oConstraints.mandatory) {
+                throw new ValidateException('Driving License cannot be empty');
+            }
+
+            return oValue;
+        };
+
+        // Model to Output
+        CustomType.prototype.formatValue = function (oValue, sInternalType) {
+            console.log('formatValue ... ' + oValue);
+
+
+            return oValue;
+
         };
 
         return CustomType;
