38957b8b6eac92004184b57bd1879e57d8ae47da
SarathChandrach <sarath.chandra@nrg.com>
2015-06-02 17:39:10 -0500

    Campaign History

--- ZE_CCUX_APP/src/nrg/view/campaign/History.controller.js
+++ ZE_CCUX_APP/src/nrg/view/campaign/History.controller.js
@@ -11,25 +11,18 @@ sap.ui.define(
 
         var Controller = CoreController.extend('nrg.view.campaign.History');
 
+        Controller.prototype.onInit = function () {
+            var oModel;
+            oModel = this.getOwnerComponent().getModel('comp-campaign');
+            if (oModel) {
+                oModel.read("/CampaignHistory('1121')");
+            }
+        };
             //TODO: Implementation required
         Controller.prototype.onBeforeRendering = function () {
-            var aPriceData, aCampaignData, aDescription;
-            aPriceData = [
-                {EFL500: "14.4000000", EFL1000: "13.4000000", EFL2000: "12.9000000" }
-            ];
-            aCampaignData = [
-                {PromoCode: "MAOPRJ", OfferCode: "50124832", TermLength: "$0.00", StartDate: "09/01/2014", EndDate: "09/01/2015", CancellationFee: "$200.00", Incentive: "$20.00" }
-            ];
-            this.getView().setModel(new sap.ui.model.json.JSONModel(), "historyData");
-            this.getView().getModel("historyData").setProperty("/priceData", aPriceData);
-            this.getView().getModel("historyData").setProperty("/campaignData", aCampaignData);
-
         };
                     //TODO: Implementation required
         Controller.prototype.onAfterRendering = function () {
-
-
-
         };
         //TODO: Implementation required
 
--- ZE_CCUX_APP/src/nrg/view/campaign/Tools.controller.js
+++ ZE_CCUX_APP/src/nrg/view/campaign/Tools.controller.js
@@ -2,12 +2,40 @@
 
 sap.ui.define(
     [
-       'nrg/util/view/BaseController'
+        'nrg/util/view/BaseController',
+        'jquery.sap.global'
     ],
 
-    function (CoreController) {
+    function (CoreController, jQuery) {
         'use strict';
 
-        return CoreController.extend('nrg.view.campaign.Tools');
+        var Controller = CoreController.extend('nrg.view.campaign.Tools');
+        Controller.prototype.onHistoryPress = function (oEvent) {
+            jQuery.sap.require("ute.ui.commons.Dialog");
+            var aHistoryView = sap.ui.view({
+                type: sap.ui.core.mvc.ViewType.XML,
+                viewName: "nrg.view.campaign.History"
+            }),
+                dialog = new ute.ui.commons.Dialog({
+                    title: '{comp-i18n>nrgCmpHisTtl}',
+                    width: '750px',
+                    height: 'auto',
+                    modal: true,
+                    content: aHistoryView,
+                    beginButton: new sap.m.Button({
+                        text: 'Close',
+                        press: function () {
+                            dialog.close();
+                        }
+                    }),
+                    afterClose: function () {
+                        dialog.destroy();
+                    }
+                });
+            //to get access to the global model
+            this.getView().addDependent(dialog);
+            dialog.open();
+        };
+        return Controller;
     }
 );
