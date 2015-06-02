eeaa09e1ec1f985cd1763a42e08c846ca38ac78d
unknown <hlin@TXHQHLIN-L1.nrgenergy.com>
2015-06-02 13:40:08 -0500

    working on search noID controller

--- ZE_CCUX_APP/src/nrg/view/dashboard/CallerNoIDSearch.controller.js
+++ ZE_CCUX_APP/src/nrg/view/dashboard/CallerNoIDSearch.controller.js
@@ -3,42 +3,71 @@
 
 sap.ui.define(
     [
-        'nrg/util/view/BaseController'
+        'nrg/util/view/BaseController',
+        'sap.ui.model.Filter',
+        'sap.ui.model.FilterOperator'
     ],
 
-    function (CoreController, JQuery) {
+    function (CoreController, Filter, FilterOperator) {
         'use strict';
 
         var Controller = CoreController.extend('nrg.view.dashboard.CallerNoIDSearch');
 
         Controller.prototype.onInit = function () {
-            //var oResultModel = new sap.ui.model.json.JSONModel('bpSearchResult'),
-            var oModel,
+            var oSearchBpODataModel,
                 oParameters,
                 oFilter,
                 aFilter;
-            this.getView().setModel(new sap.ui.model.json.JSONModel(), 'bpSearchResult');
 
-            oModel = this.getOwnerComponent().getModel('comp-dashboard');
 
-            if (oModel) {
-                oFilter = new sap.ui.model.Filter("PartnerID", sap.ui.model.FilterOperator.EQ, "1121");
-                aFilter = [];
-                aFilter.push(oFilter);
-                oParameters = {
-                    filters : aFilter,
-                    success : function (oData) {
-                        if (oData.results) {
-                            this.getView().getModel('bpSearchResult').setData(oData.results);
-                        }
-                    }.bind(this),
-                    error: function (oError) {
-                        this.getView().getModel('bpSearchResult').setData(null);
+
+            /*Models in the controller*/
+
+            //get OData Model from component level first
+            this.getView().setModel(this.getOwnerComponent().getModel('comp-dashboard'), 'oSearchBpODataModel');
+            //JSON model for search result
+            this.getView().setModel(new sap.ui.model.json.JSONModel(), 'oBpSearchResult');
+
+
+        };
+
+        Controller.prototype.onSearch = function () {
+            this._searchBP('/BpSearchs', this._createSearchParameters());
+        };
+
+        Controller.prototype._createSearchParameters = function () {
+            var oFilter, aFilter, oParameters;
+
+
+            /*This need to be re-written*/
+            oFilter = new Filter("PartnerID", FilterOperator.EQ, "1121");
+            aFilter = [];
+            aFilter.push(oFilter);
+
+            oParameters = {
+                filters : aFilter,
+                success : function (oData) {
+                    if (oData.results) {
+                        this.getView().getModel('oBpSearchResult').setData(oData.results);
                     }
-                };
-                oModel.read('/BpSearchs', oParameters);
+                }.bind(this),
+                error: function (oError) {
+                    this.getView().getModel('oBpSearchResult').setData(null);
+                }.bind(this)
+            };
+
+            return oParameters;
+        };
+
+
+        Controller.prototype._searchBP = function (sPath, oParameters) {
+            var oModel = this.getView().getModel('oSearchBpODataModel');
+            if (oModel) {
+                oModel.read(sPath, oParameters);
             }
         };
+
+        //Controller.prototype.
 
         return Controller;
     }
