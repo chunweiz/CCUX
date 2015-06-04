b9f38475bd6952e27f52750e5a27cb1b8358e058
unknown <hlin@TXHQHLIN-L1.nrgenergy.com>
2015-06-04 17:13:27 -0500

    Working on verification page

---
+++ ZE_CCUX_APP/src/nrg/view/dashboard/CustomerDataSummary.controller.js
@@ -0,0 +1,19 @@
+/*globals sap*/
+/*jslint nomen:true*/
+
+sap.ui.define(
+    [
+        'nrg/util/view/BaseController'
+    ],
+
+    function (CoreController) {
+        'use strict';
+
+        var Controller = CoreController.extend('nrg.view.dashboard.CustomerDataSummary');
+
+        Controller.prototype.onInit = function () {
+        };
+
+        return Controller;
+    }
+);
