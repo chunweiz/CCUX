5b41dad8e594176df2f5f654cd424940665e7708
SarathChandrach <sarath.chandra@nrg.com>
2015-06-04 15:24:15 -0500

    campaign changes

--- ZE_CCUX_APP/src/nrg/view/campaign/Overview.controller.js
+++ ZE_CCUX_APP/src/nrg/view/campaign/Overview.controller.js
@@ -3,29 +3,95 @@
 
 sap.ui.define(
     [
-        'nrg/util/view/BaseController'
+        'nrg/util/view/BaseController',
+        'sap/ui/model/Filter',
+        'sap/ui/model/FilterOperator',
+        'jquery.sap.global'
     ],
 
-    function (CoreController) {
+    function (CoreController, Filter, FilterOperator, jQuery) {
         'use strict';
 
         var Controller = CoreController.extend('nrg.view.campaign.Overview');
 
         Controller.prototype.onInit = function () {
-            var oModel;
-            oModel = this.getOwnerComponent().getModel('comp-campaign');
-            if (oModel) {
-                oModel.read("/CampaignCurrent('1121')");
-            }
-        };
+            var oModel,
+                oContext,
+                sPath,
+                oParameters,
+                aFilters = this._createSearchFilterObject("1121", "Y");
 
+            sPath = "/CpgCurPndSet";
+            oModel = this.getOwnerComponent().getModel('comp-campaign');
+            oParameters = {
+                filters : aFilters,
+                success : function (oData) {
+                    if (oData.results) {
+                        oContext = this.getView().getModel('comp-campaign').getContext("/CpgCurPndSet(CampaignID='CampaignID 1',PromoCode='PromoCode 1')");
+                        this.getView().setBindingContext(oContext, "oCmpCP");
+                        this.getView().bindElement("/CpgCurPndSet(CampaignID='CampaignID 1',PromoCode='PromoCode 1')");
+                        this.getView().bindObject("/CpgCurPndSet(CampaignID='CampaignID 1',PromoCode='PromoCode 1')");
+                        jQuery.sap.log.info("Odata Read Successfully");
+                    }
+                }.bind(this),
+                error: function (oError) {
+                    jQuery.sap.log.info("Some Error");
+                }.bind(this)
+            };
+            if (oModel) {
+                oModel.read(sPath, oParameters);
+            }
+
+        };
+        Controller.prototype._createSearchFilterObject = function (oContractID, oCurrentFlag) {
+            var aFilters = [],
+                oFilterTemplate = new Filter();
+
+
+            oFilterTemplate.sPath = 'ContractID';
+            oFilterTemplate.sOperator = FilterOperator.EQ;
+            oFilterTemplate.oValue1 = oContractID;
+            aFilters.push(oFilterTemplate);
+
+            oFilterTemplate.sPath = 'CurrentFlag';
+            oFilterTemplate.sOperator = FilterOperator.EQ;
+            oFilterTemplate.oValue1 = oCurrentFlag;
+            aFilters.push(oFilterTemplate);
+
+            return aFilters;
+        };
             //TODO: Implementation required
         Controller.prototype.onBeforeRendering = function () {
         };
                     //TODO: Implementation required
         Controller.prototype.onAfterRendering = function () {
-        };
+            var oModel;
+            oModel = this.getView().getModel('OV-cmp');
 
+        };
+        Controller.prototype.toggleCampaign = function () {
+            var oModel,
+                sPath = "/CpgCurPndSet",
+                oParameters,
+                aFilters = this._createSearchFilterObject("1121", "N");
+            oModel = this.getOwnerComponent().getModel('comp-campaign');
+            oParameters = {
+                filters : aFilters,
+                success : function (oData) {
+                    if (oData.results) {
+                        //this.getView().bindElement('/0');
+                        jQuery.sap.log.info("Odata Read Successfully");
+                    }
+                }.bind(this),
+                error: function (oError) {
+                    jQuery.sap.log.info("Some Error");
+                }.bind(this)
+            };
+            if (oModel) {
+                oModel.read(sPath, oParameters);
+            }
+
+        };
 
         return Controller;
     }
