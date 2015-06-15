4996b7b1e1e677e024ac2115db4a65b7e69e51bb
SarathChandrach <sarath.chandra@nrg.com>
2015-06-15 15:29:29 -0500

    Campaign changes offers

--- ZEMOD_CMPGN/src/nrg/module/campaign/view/Overview.controller.js
+++ ZEMOD_CMPGN/src/nrg/module/campaign/view/Overview.controller.js
@@ -14,30 +14,48 @@ sap.ui.define(
 
         var Controller = CoreController.extend('nrg.module.campaign.view.Overview');
 
+
+		/* =========================================================== */
+		/* lifecycle method- Init                                     */
+		/* =========================================================== */
         Controller.prototype.onInit = function () {
+            this.getOwnerComponent().getRouter().getRoute("campaign").attachPatternMatched(this._onObjectMatched, this);
+
+        };
+        /* =========================================================== */
+		/* lifecycle method- After Rendering                          */
+		/* =========================================================== */
+        Controller.prototype.onAfterRendering = function () {
+        };
+		/**
+		 * Binds the view to the object path and expands the aggregated line items.
+		 *
+		 * @function
+		 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
+		 * @private
+		 */
+        Controller.prototype._onObjectMatched = function (oEvent) {
             var oModel,
-                oContext,
                 sCurrentPath,
                 sEligibilityPath,
-                oParameters,
-                sEligibilityModel,
-                aToggleContainer,
-                aToggleTemplate,
+                mParameters,
+                oToggleContainer,
+                oToggleTemplate,
                 aFilters = this._createSearchFilterObject("1121", "Y");
 
             sCurrentPath = this.getOwnerComponent().getModel("comp-i18n-campaign").getProperty("nrgCurrentPendingSet");
             sEligibilityPath = this.getOwnerComponent().getModel("comp-i18n-campaign").getProperty("nrgEligibilitySet");
             oModel = this.getOwnerComponent().getModel('comp-campaign');
-            aToggleContainer = this.getView().byId("idnrgCamToggleT");
-            aToggleTemplate = this.getView().byId("idCamToggleBtn").clone();
-            oParameters = {
+            oToggleContainer = this.getView().byId("idnrgCamToggleT");
+            oToggleTemplate = this.getView().byId("idCamToggleBtn").clone();
+            mParameters = {
                 model : "comp-campaign",
                 path : sCurrentPath,
-                template : aToggleTemplate,
+                template : oToggleTemplate,
                 filters : aFilters
             };
-            aToggleContainer.bindAggregation("content", oParameters);
-            oParameters = {
+            oToggleContainer.bindAggregation("content", mParameters);
+            mParameters = {
                 filters : aFilters,
                 success : function (oData) {
                     this.getView().byId("idCamCustReqOfferBtn").bindElement({
@@ -55,40 +73,48 @@ sap.ui.define(
                 }.bind(this)
             };
             if (oModel) {
-                oModel.read(sEligibilityPath, oParameters);
+                oModel.read(sEligibilityPath, mParameters);
             }
             this.getView().setModel(oModel, "Overview-elig");
+
+		};
+
+        /**
+		 * Binds the view to the object path. Makes sure that detail view displays
+		 * a busy indicator while data for the corresponding element binding is loaded.
+		 *
+		 * @function
+		 * @param {string} sObjectPath path to the object to be bound
+		 * @private
+		 */
+		Controller.prototype._bindView = function (sObjectPath) {
+
         };
 
-        Controller.prototype._createSearchFilterObject = function (oContractID, oCurrentFlag) {
+        /**
+		 * Assign the filter objects based on the input selection
+		 *
+		 * @function
+		 * @param {sContractID} Contract to be used aa a filter
+         * @param {sCurrentFlag} Filter flag to determine the current
+		 * @private
+		 */
+        Controller.prototype._createSearchFilterObject = function (sContractID, sCurrentFlag) {
             var aFilters = [],
                 oFilterTemplate = new Filter();
             oFilterTemplate.sPath = 'Contract';
             oFilterTemplate.sOperator = FilterOperator.EQ;
-            oFilterTemplate.oValue1 = oContractID;
+            oFilterTemplate.oValue1 = sContractID;
             aFilters.push(oFilterTemplate);
             return aFilters;
         };
 
-        Controller.prototype.onAfterRendering = function () {
-            var aContent, abinding, sPath, that = this,
-                aToggleContainer = this.getView().byId("idnrgCamToggleT"),
-                handler = function () {
-                    aContent = aToggleContainer.getContent();
-                    if ((aContent !== undefined) && (aContent.length > 0)) {
-                        sPath = aContent[0].getBindingContext("comp-campaign").getPath();
-                       // aContent[0].addStyleClass("nrgCamHisBut-Selected");
-                        that.getView().bindElement({
-                            model : "comp-campaign",
-                            path : sPath
-                        });
-                    }
-                    abinding.detachDataReceived(handler);
-                };
-            abinding = aToggleContainer.getBinding("content");
-            abinding.attachDataReceived(handler);
-        };
-
+        /**
+		 * Toggles between Current and Pending clicks
+		 *
+		 * @function
+         * @param {sap.ui.base.Event} oEvent pattern match event
+		 */
         Controller.prototype.toggleCampaign = function (oEvent) {
             var sPath;
             sPath = oEvent.getSource().getBindingContext("comp-campaign").getPath();
@@ -98,10 +124,14 @@ sap.ui.define(
             });
         };
 
+        /**
+		 * Traverse to Offers View when the user selected Agent requested and Customer Requested buttons
+		 *
+		 * @function
+         * @param {sap.ui.base.Event} oEvent pattern match event
+		 */
         Controller.prototype.onOffers = function (oEvent) {
-            var oParameters = {
 
-            };
             this.navTo("campaignoffers", {bpNum: "123", caNum: "1234"});
         };
         return Controller;
