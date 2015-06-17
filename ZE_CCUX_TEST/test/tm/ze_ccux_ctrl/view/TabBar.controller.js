6af1781fea4adbf76171b1baeb053a66f0659293
tmhew <tauming.hew@utegration.com>
2015-06-17 18:10:12 -0500

    changed from selectedKey to selectedItem

--- ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/TabBar.controller.js
+++ ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/TabBar.controller.js
@@ -3,26 +3,50 @@
 sap.ui.define(
     [
         'jquery.sap.global',
-        'sap/ui/core/mvc/Controller'
+        'sap/ui/core/mvc/Controller',
+        'ute/ui/main/Label',
+        'ute/ui/main/TabBarItem'
     ],
 
-    function (jQuery, Controller) {
+    function (jQuery, Controller, Label, TabBarItem) {
         'use strict';
 
         var CustomController = Controller.extend('test.tm.ze_ccux_ctrl.view.TabBar');
+
+        CustomController.prototype.onInit = function () {
+
+            var oTabBar = this.getView().byId('nrgTabBar');
+
+
+
+            var oLabel = new Label({ text: 'this is a label' });
+            var oItem = new TabBarItem({
+                content: oLabel,
+                press: function (oControlEvent) {
+                    alert('try me');
+                }
+            });
+
+            oTabBar.addContent(oItem);
+
+            oLabel = new Label({ text: 'this is another label' });
+            oItem = new TabBarItem({
+                content: oLabel,
+                press: function (oControlEvent) {
+                    alert('try another me');
+                }
+            });
+
+            oTabBar.addContent(oItem);
+
+        };
 
         CustomController.prototype.onPressed = function (oControlEvent) {
             console.log("onPressed ... " + oControlEvent.getSource().getSelected());
         };
 
         CustomController.prototype.onSelected = function (oControlEvent) {
-            var aContent = oControlEvent.getSource().getContent();
-
-            aContent.forEach(function (oContent) {
-                if (oContent instanceof ute.ui.main.TabBarItem) {
-                    console.log(oContent.getId() + ' ... ' + oContent.getSelected());
-                }
-            });
+            console.log(oControlEvent.getParameter('selectedItem').getKey());
         };
 
         return CustomController;
