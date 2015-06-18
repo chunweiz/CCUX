576b644d9eeedfc5913817431b9f59c1c614b46a
tmhew <tauming.hew@utegration.com>
2015-06-18 14:46:39 -0500

    sicf path

--- ZEBASE/src/nrg/base/component/RealDataManager.js
+++ ZEBASE/src/nrg/base/component/RealDataManager.js
@@ -44,11 +44,13 @@ sap.ui.define(
         };
 
         Manager.prototype._addModuleODataModels = function (oDataReal) {
-            var sDataReal, oModel;
+            var sDataReal, oModel, sRelativeToSicf;
+
+            sRelativeToSicf = '../../../../../../../../../';
 
             for (sDataReal in oDataReal) {
                 if (oDataReal.hasOwnProperty(sDataReal)) {
-                    oModel = new ODataModel(oDataReal[sDataReal].url, true);
+                    oModel = new ODataModel(sRelativeToSicf + oDataReal[sDataReal].url, true);
                     this._oComponent.setModel(oModel, sDataReal);
                 }
             }
