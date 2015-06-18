77716c58cf5d4b7e6761bd43a423f4e78e568107
tmhew <tauming.hew@utegration.com>
2015-06-18 17:16:57 -0500

    chaione popup

---
+++ ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/Popup.controller.js
@@ -0,0 +1,27 @@
+/*global sap*/
+
+sap.ui.define(
+    [
+        'sap/ui/core/mvc/Controller'
+    ],
+
+    function (Controller) {
+        'use strict';
+
+        var CustomController = Controller.extend('test.tm.ze_ccux_ctrl.view.Popup');
+
+        CustomController.prototype.onInit = function () {
+
+        };
+
+        CustomController.prototype.onBeforeRendering = function () {
+
+        };
+
+        CustomController.prototype.onAfterRendering = function () {
+
+        };
+
+        return CustomController;
+    }
+);
