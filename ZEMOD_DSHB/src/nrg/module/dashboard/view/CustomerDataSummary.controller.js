5351845015fe0f852c5bdd5b8bec7e8179d01358
unknown <hlin@TXHQHLIN-L1.nrgenergy.com>
2015-06-18 14:57:56 -0500

    Verify Summary Controller

--- ZEMOD_DSHB/src/nrg/module/dashboard/view/CustomerDataSummary.controller.js
+++ ZEMOD_DSHB/src/nrg/module/dashboard/view/CustomerDataSummary.controller.js
@@ -3,15 +3,20 @@
 
 sap.ui.define(
     [
-        'nrg/base/view/BaseController'
+        'nrg/base/view/BaseController',
+        'sap/ui/core/routing/HashChanger'
     ],
 
-    function (CoreController) {
+    function (CoreController, HashChanger) {
         'use strict';
 
         var Controller = CoreController.extend('nrg.module.dashboard.view.CustomerDataSummary');
 
         Controller.prototype.onInit = function () {
+            //Get the hash to retrieve bp #
+            var oHashChanger = new HashChanger(),
+                sUrlHash = oHashChanger.getHash();
+
         };
 
         return Controller;
--- ZEMOD_DSHB/src/nrg/module/dashboard/view/CustomerDataVerification.controller.js
+++ ZEMOD_DSHB/src/nrg/module/dashboard/view/CustomerDataVerification.controller.js
@@ -1,9 +1,25 @@
 /*global sap*/
 
 sap.ui.define(
-    [],
+    [
+        'nrg/base/view/BaseController',
+        'sap/ui/model/Filter',
+        'sap/ui/model/FilterOperator',
+        'sap/ui/core/routing/HashChanger'
+    ],
 
-    function () {
+    function (CoreController, Filter, FilterOperator, HashChanger) {
         'use strict';
+
+        var Controller = CoreController.extend('nrg.module.dashboard.view.CustomerDataVerification');
+
+        Controller.prototype.onInit = function () {
+            //Get the hash to retrieve bp #
+            var oHashChanger = new HashChanger(),
+                sUrlHash = oHashChanger.getHash();
+
+
+
+        };
     }
 );
