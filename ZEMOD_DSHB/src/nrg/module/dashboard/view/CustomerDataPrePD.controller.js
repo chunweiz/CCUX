6087275a89a5165860902a6ab3c72a8aadb7ee01
gtang <grant.tang@utegration.com>
2015-06-29 15:30:08 -0500

    init the controller for BillingInfo and PrePD

--- ZEMOD_DSHB/src/nrg/module/dashboard/view/CustomerDataPrePD.controller.js
+++ ZEMOD_DSHB/src/nrg/module/dashboard/view/CustomerDataPrePD.controller.js
@@ -1,10 +1,33 @@
-/*global sap*/
+/*globals sap*/
+/*jslint nomen:true*/
 
 sap.ui.define(
-    [],
+    [
+        'jquery.sap.global',
+        'sap/ui/core/mvc/Controller',
+        'sap/ui/model/json/JSONModel'
+    ],
 
-    function () {
+    function (jQuery, Controller, JSONModel) {
         'use strict';
 
+        var CustomController = Controller.extend('nrg.module.dashboard.view.CustomerDataPrePD');
+
+        CustomController.prototype.onInit = function () {
+        };
+
+        CustomController.prototype.onBeforeRendering = function () {
+
+        };
+
+        CustomController.prototype.onAfterRendering = function () {
+
+        };
+
+        CustomController.onExit = function () {
+
+        };
+
+        return CustomController;
     }
 );
