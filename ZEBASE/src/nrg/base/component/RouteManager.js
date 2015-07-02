e04868b403c96d30f8503f11f59987d4aab60ade
tmhew <tauming.hew@utegration.com>
2015-07-02 10:47:10 -0500

    Routemanager to manage routing callbacks

---
+++ ZEBASE/src/nrg/base/component/RouteManager.js
@@ -0,0 +1,79 @@
+/*global sap*/
+/*jslint nomen:true*/
+
+sap.ui.define(
+    [
+        'jquery.sap.global',
+        'sap/ui/base/Object',
+        'nrg/base/component/WebUiManager'
+    ],
+
+    function (jQuery, BaseObject, WebUiManager) {
+        'use strict';
+
+        var Manager = BaseObject.extend('nrg.base.component.RouteManager', {
+            constructor: function (oComponent) {
+                BaseObject.apply(this);
+                this._oComponent = oComponent;
+            },
+
+            metadata: {
+                publicMethods: [
+                    'init'
+                ]
+            }
+        });
+
+        Manager.prototype.init = function () {
+            this._registerRouteCallback();
+            this._oComponent.getRouter().initialize();
+            this._subscribeWebUi();
+        };
+
+        Manager.prototype._subscribeWebUi = function () {
+            var oEventBus = sap.ui.getCore().getEventBus();
+            oEventBus.subscribe(WebUiManager.EventBus.Subscribe.Channel, 'NAVIGATE', this._onWebUiEvent, this);
+        };
+
+        Manager.prototype._unsubscribeWebUi = function () {
+            var oEventBus = sap.ui.getCore().getEventBus();
+            oEventBus.unsubscribe(WebUiManager.EventBus.Subscribe.Channel, 'NAVIGATE', this._onWebUiEvent, this);
+        };
+
+        Manager.prototype._onWebUiEvent = function (sChannelId, sEventId, oData) {
+            var oRouter, sRouteName, oRouteParams;
+
+            if (!oData || !oData.ROUTE) {
+                return;
+            }
+
+            sRouteName = oData.ROUTE;
+            oRouteParams = oData.PARAMS || null;
+
+            oRouter = this._oComponent.getRouter();
+            oRouter.navTo(sRouteName, oRouteParams, false);
+        };
+
+        Manager.prototype._registerRouteCallback = function () {
+            var oRoute, sRouteName;
+
+            oRoute = this._oComponent.getRoutes();
+            for (sRouteName in oRoute) {
+                if (oRoute.hasOwnProperty(sRouteName)) {
+                    oRoute[sRouteName].callback = this._onRouteCallback.bind(this);
+                }
+            }
+        };
+
+        Manager.prototype._onRouteCallback = function (route, args, config, targetControl, view) {
+            jQuery.sap.log.info('Navigating to route ' + route);
+        };
+
+        Manager.prototype.destroy = function () {
+            this._unsubscribeWebUi();
+            BaseObject.prototype.destroy.apply(this, arguments);
+        };
+
+        return Manager;
+    }
+);
