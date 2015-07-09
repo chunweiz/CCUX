7c230cc2e326ae500d19fe63e0ffe8e77bd04eda
tmhew <tauming.hew@utegration.com>
2015-07-08 23:42:11 -0500

    xml templating with odata

--- ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/XMLView.controller.js
+++ ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/XMLView.controller.js
@@ -12,42 +12,45 @@ sap.ui.define(
         var CustomController = Controller.extend('test.tm.ze_ccux_ctrl.view.XMLView');
 
         CustomController.prototype.onInit = function () {
-            var oCompModel, oTemplateModel, oViewTemplate, oDataTemplateModel;
 
-            oTemplateModel = new JSONModel({
-                meta: {
-                    showNrg: true,
-                    testRepeat: [
-                        { value: 'value001' },
-                        { value: 'value002' },
-                        { value: 'value003' }
-                    ]
-                }
-            });
-
-            oViewTemplate = sap.ui.view({
-                preprocessors: {
-                    xml: {
-                        models: {
-                            tmpl: oTemplateModel
-                        }
-                    }
-                },
-                type: sap.ui.core.mvc.ViewType.XML,
-                viewName: 'test.tm.ze_ccux_ctrl.view.XMLTemplate'
-            });
-
-            this.getView().byId('viewContent').addContent(oViewTemplate);
         };
 
-        CustomController.prototype.onAfterRendering = function () {
-            var oModel = this.getOwnerComponent().getModel('comp-odata');
+        CustomController.prototype.onBeforeRendering = function () {
+            var oDataModel, mParams;
 
-            oModel.read('/BusinessPartnerSet', {
-                success: function (oData) {
+            mParams = {};
+            mParams.success = function (oData) {
+                var oJsonData, oTemplateModel, oViewTemplate;
 
-                }
-            });
+                oTemplateModel = new JSONModel({
+                    meta: {
+                        showNrg: true,
+                        testRepeat: [
+                            { value: 'value001' },
+                            { value: 'value002' },
+                            { value: 'value003' }
+                        ]
+                    },
+                    results: oData.results
+                });
+
+                oViewTemplate = sap.ui.view({
+                    preprocessors: {
+                        xml: {
+                            models: {
+                                tmpl: oTemplateModel
+                            }
+                        }
+                    },
+                    type: sap.ui.core.mvc.ViewType.XML,
+                    viewName: 'test.tm.ze_ccux_ctrl.view.XMLTemplate'
+                });
+
+                this.getView().byId('viewContent').addContent(oViewTemplate);
+            }.bind(this);
+
+            oDataModel = this.getOwnerComponent().getModel('comp-odata');
+            oDataModel.read('/BusinessPartnerSet', mParams);
         };
 
         return CustomController;
