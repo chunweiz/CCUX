0e6581a96607464dafb2277e03fcbf14527e30ac
tmhew <tauming.hew@utegration.com>
2015-08-17 14:55:19 -0500

    data loss popup

---
+++ ZEBASE/src/nrg/base/component/Router.js
@@ -0,0 +1,48 @@
+/*global sap*/
+
+sap.ui.define(
+    [
+        'sap/ui/core/routing/Router'
+    ],
+
+    function (Router) {
+        'use strict';
+
+        var CustomRouter = Router.extend('nrg.base.component.Router', {
+            constructor: function (oRoutes, oConfig, oOwner, oTargetsConfig) {
+                this._oComponent = oOwner;
+
+                Router.apply(this, arguments);
+            }
+        });
+
+        CustomRouter.prototype.navTo = function (sName, oParameters, bReplace) {
+            var oCcuxApp, fnConfirmCallback;
+
+            oCcuxApp = this._oComponent.getCcuxApp();
+
+            if (oCcuxApp.isInEdit()) {
+                fnConfirmCallback = function (sAction) {
+                    if (sAction === ute.ui.main.Popup.Action.Yes) {
+                        oCcuxApp.setInEdit(false);
+                        Router.prototype.navTo.call(this, sName, oParameters, bReplace);
+                    }
+                }.bind(this);
+
+                ute.ui.main.Popup.Confirm({
+                    title: 'Possible Data Loss',
+                    message: 'Do you want to continue?',
+                    callback: fnConfirmCallback
+                });
+
+            } else {
+                Router.prototype.navTo.apply(this, arguments);
+            }
+        };
+
+
+        return CustomRouter;
+    },
+
+    true
+);
