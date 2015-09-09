07516df592c618c59118dfb45a13243ec8503a72
tmhew <tauming.hew@utegration.com>
2015-09-09 16:20:30 -0500

    chart

--- ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/CustJourneyChannelChart.js
+++ ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/CustJourneyChannelChart.js
@@ -155,7 +155,7 @@ sap.ui.define(
                     .append('polyline')
                         .attr('points', function(data) {
                             var aXY = fnLineOuterArc.centroid(data);
-                            aXY[0] = iRadius * (fnMidAngle(data) < Math.PI ? 1 : -1 );
+                            aXY[0] = iRadius * 1.4 * (fnMidAngle(data) < Math.PI ? 1 : -1 );
 
                             return [fnLineInnerArc.centroid(data), fnLineOuterArc.centroid(data), aXY];
                         })
@@ -184,7 +184,7 @@ sap.ui.define(
                         .attr('dy', '0.35em')
                         .attr('transform', function(data) {
                             var aXY = fnLineOuterArc.centroid(data);
-                            aXY[0] = iRadius * 1.1 * (fnMidAngle(data) < Math.PI ? 1 : -1 );
+                            aXY[0] = iRadius * 1.5 * (fnMidAngle(data) < Math.PI ? 1 : -1 );
                             return 'translate(' + aXY + ')';
                         })
                         .style('text-anchor', function(data) {
