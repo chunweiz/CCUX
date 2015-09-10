6a21de4cec32d6e29eed0c4cef31f2042c2dddbb
tmhew <tauming.hew@utegration.com>
2015-09-10 08:53:47 -0500

    chart

---
+++ ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/UsageHistoryChart.js
@@ -0,0 +1,20 @@
+/*global sap*/
+
+sap.ui.define(
+    [
+        'sap/ui/core/Control'
+    ],
+
+    function (Control) {
+        'use strict';
+
+        var CustomControl = Control.extend('test.tm.ze_ccux_ctrl.view.UsageHistoryChart', {
+            metadata: {
+
+            }
+        });
+
+
+        return CustomControl;
+    }
+);
