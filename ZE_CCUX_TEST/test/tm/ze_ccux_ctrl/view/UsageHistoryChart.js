704af6a279b89a6b8decb1daeab6c91d14909e41
tmhew <tauming.hew@utegration.com>
2015-09-14 13:10:59 -0500

    x grid

--- ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/UsageHistoryChart.js
+++ ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/UsageHistoryChart.js
@@ -110,7 +110,12 @@ sap.ui.define(
                 .call(fnConsumptionYAxis);
 
             // X grid
-            var fnConsumptionXGrid = d3.svg.axis();
+            var fnConsumptionXGrid = d3.svg.axis()
+                .orient('bottom')
+                .scale(fnScaleX)
+                .ticks(aDataSet.length)
+                .tickSize(iHeight, 0, 0)
+                .tickFormat('');
 
             oCanvas.append('g')
                 .attr('class', 'tmUsageHistChart-consumptionXGrid')
