5176ba3462b61017fc7458df00b4c85f7566c2fe
tmhew <tauming.hew@utegration.com>
2015-09-16 10:55:57 -0500

    footer notification

--- ZECTRL_APP/src/ute/ui/app/FooterNotificationCenterRenderer.js
+++ ZECTRL_APP/src/ute/ui/app/FooterNotificationCenterRenderer.js
@@ -21,7 +21,56 @@ sap.ui.define(
         };
 
         CustomRenderer._renderContent = function (oRm, oCustomControl) {
+            this._renderLeftContent(oRm, oCustomControl);
+            this._renderRightContent(oRm, oCustomControl);
+        };
 
+        CustomRenderer._renderLeftContent = function (oRm, oCustomControl) {
+            oRm.write('<div');
+            oRm.addClass('uteAppFtrNotifCtr-cnt');
+            oRm.addClass('uteAppFtrNotifCtr-leftCnt');
+            oRm.writeClasses();
+            oRm.write('>');
+
+            //Render odd items
+            oCustomControl.getContent().forEach(function (item, index) {
+                if ((index + 1) % 2 === 1) {
+                    oRm.write('<div');
+                    oRm.addClass('uteAppFtrNotifCtr-cntItem');
+                    oRm.writeClasses();
+                    oRm.write('>');
+
+                    oRm.renderControl(item);
+
+                    oRm.write('</div>');
+                }
+            }, this);
+
+            oRm.write('</div>');
+        };
+
+        CustomRenderer._renderRightContent = function (oRm, oCustomControl) {
+            oRm.write('<div');
+            oRm.addClass('uteAppFtrNotifCtr-cnt');
+            oRm.addClass('uteAppFtrNotifCtr-rightCnt');
+            oRm.writeClasses();
+            oRm.write('>');
+
+            //Render even items
+            oCustomControl.getContent().forEach(function (item, index) {
+                if ((index + 1) % 2 === 0) {
+                    oRm.write('<div');
+                    oRm.addClass('uteAppFtrNotifCtr-cntItem');
+                    oRm.writeClasses();
+                    oRm.write('>');
+
+                    oRm.renderControl(item);
+
+                    oRm.write('</div>');
+                }
+            }, this);
+
+            oRm.write('</div>');
         };
 
         return CustomRenderer;
--- ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/FooterNotification.controller.js
+++ ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/FooterNotification.controller.js
@@ -12,7 +12,14 @@ sap.ui.define(
         var CustomController = Controller.extend('test.tm.ze_ccux_ctrl.view.FooterNotification');
 
         CustomController.prototype.onBeforeRendering = function () {
-            
+            this.getView().setModel(new JSONModel({
+                notification: [
+                    { design: 'Error', text: 'Winter purslane courgette pumpkin quandong komatsuna fennel green bean cucumber watercress.' },
+                    { design: 'Warning', text: 'Caulie dandelion maize lentil collard greens radish arugula sweet pepper water spinach kombu courgette lettuce.' },
+                    { design: 'Success', text: 'Parsnip lotus root celery yarrow seakale tomato collard greens tigernut epazote ricebean melon tomatillo soybean chicory broccoli beet greens peanut salad.' },
+                    { design: 'Information', text: 'Peanut gourd nori welsh onion rock melon mustard jícama. Desert raisin amaranth kombu aubergine kale seakale brussels sprout pea.' }
+                ]
+            }), 'view-data');
         };
 
         return CustomController;
