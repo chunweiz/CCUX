fc138287ad1c1eeac75eb8db17b2dbe508706b12
tmhew <tauming.hew@utegration.com>
2015-09-21 17:53:10 -0500

    timeline grouping

--- ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/control/CustomerJourneyTimelineChannel.js
+++ ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/control/CustomerJourneyTimelineChannel.js
@@ -39,11 +39,46 @@ sap.ui.define(
             Correspondence: 'Correspondence'
         };
 
+        CustomControl.prototype._aGroupRegistry = {};
+
+        CustomControl.prototype.exit = function () {
+            var sSelectionGroup = this.getSelectionGroup();
+
+            if (!sSelectionGroup || !this._aGroupRegistry[sSelectionGroup]) {
+                return;
+            }
+
+            var iIndex = this._aGroupRegistry[sSelectionGroup].indexOf(this);
+            if (iIndex && iIndex !== -1) {
+                this._aGroupRegistry[sSelectionGroup].splice(iIndex, 1);
+            }
+        };
+
+        CustomControl.prototype.setSelectionGroup = function (sSelectionGroup) {
+            if (!sSelectionGroup) {
+                return;
+            }
+
+            if (!this._aGroupRegistry[sSelectionGroup]) {
+                this._aGroupRegistry[sSelectionGroup] = [];
+            }
+
+            this._aGroupRegistry[sSelectionGroup].push(this);
+        };
+
+        CustomControl.prototype.setSelected = function (bSelected) {
+            bSelected = !!bSelected;
+
+            console.log(this._aGroupRegistry);
+
+            this.setProperty('selected', bSelected, true);
+        };
+
         CustomControl.prototype.onclick = function (oEvent) {
             this._onDoubleClick = false;
 
             // Wait for a while to check whether user intention is to double click.
-            //  If it is, do not fire single click.
+            // If it is, do not fire single click.
             jQuery.sap.delayedCall(300, this, function () {
                 if (!this._onDoubleClick) {
                     this.firePress();
@@ -53,6 +88,7 @@ sap.ui.define(
 
         CustomControl.prototype.ondblclick = function (oEvent) {
             this._onDoubleClick = true;
+            this.setSelected(true);
             this.fireDoublePress();
         };
 
