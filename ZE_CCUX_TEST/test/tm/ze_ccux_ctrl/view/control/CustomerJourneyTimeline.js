c9dba2d9570ac4cba32545adc65aa3a8a1d7681b
tmhew <tauming.hew@utegration.com>
2015-09-21 14:47:56 -0500

    scrolling

--- ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/control/CustomerJourneyTimeline.js
+++ ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/control/CustomerJourneyTimeline.js
@@ -11,10 +11,6 @@ sap.ui.define(
 
         var CustomControl = Control.extend('test.tm.ze_ccux_ctrl.view.control.CustomerJourneyTimeline', {
             metadata: {
-                properties: {
-                    width: { type: 'sap.ui.core.CSSSize', defaultValue: '680px' }
-                },
-
                 aggregations: {
                     channel: {
                         type: 'test.tm.ze_ccux_ctrl.view.control.CustomerJourneyTimelineChannel',
@@ -28,6 +24,73 @@ sap.ui.define(
 
             renderer: CustomRenderer
         });
+
+        CustomControl.SCROLL_STEP = 250;
+
+        CustomControl.prototype.onBeforeRendering = function () {
+            this.$('navBack').unbind('click', this._onNavBackClick);
+            this.$('navFwd').unbind('click', this._onNavForwardClick);
+        };
+
+        CustomControl.prototype.onAfterRendering = function () {
+            this.$('navBack').bind('click', this._onNavBackClick.bind(this));
+            this.$('navFwd').bind('click', this._onNavForwardClick.bind(this));
+        };
+
+        CustomControl.prototype.exit = function () {
+            this.$('navBack').unbind('click', this._onNavBackClick);
+            this.$('navFwd').unbind('click', this._onNavForwardClick);
+        };
+
+        CustomControl.prototype._onNavBackClick = function (oEvent) {
+            this._scroll(-CustomControl.SCROLL_STEP, 500);
+        };
+
+        CustomControl.prototype._onNavForwardClick = function (oEvent) {
+            this._scroll(CustomControl.SCROLL_STEP, 500);
+        };
+
+        /* css zigzag border - http://codepen.io/pouretrebelle/pen/hypGk */
+
+        CustomControl.prototype._checkOverflow = function (oTimelineDomRef, oBackArrow, oForwardArrow) {
+            var bScrollBack = false;
+            var bScrollForward = false;
+
+            var iScrollLeft = oTimelineDomRef.scrollLeft;
+            var iRealWidth = oTimelineDomRef.scrollWidth;
+			var iAvailableWidth = oTimelineDomRef.clientWidth; // Viewport
+
+            if (Math.abs(iRealWidth - iAvailableWidth) === 1) {
+				iRealWidth = iAvailableWidth;
+			}
+
+            if (iScrollLeft > 0) {
+                bScrollBack = true;
+            }
+
+            if ((iRealWidth > iAvailableWidth) && (iScrollLeft + availableWidth < realWidth)) {
+                bScrollForward = true;
+            }
+
+            if (bScrollBack) {
+                // Enable oBackArrow
+            }
+
+            if (bScrollForward) {
+                // Enable oForwardArrow;
+            }
+
+        };
+
+        CustomControl.prototype._scroll = function (iDelta, iDuration) {
+            var oDomRef = this.getDomRef('container');
+    		var iScrollLeft = oDomRef.scrollLeft;
+    		var iScrollTarget = iScrollLeft + iDelta;
+
+    		jQuery(oDomRef).stop(true, true).animate({
+                scrollLeft: iScrollTarget
+            }, iDuration);
+        };
 
         return CustomControl;
     }
