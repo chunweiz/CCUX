21c18513dbf1aa45edcba367c9446629bc4c0575
tmhew <tauming.hew@utegration.com>
2015-09-21 17:24:38 -0500

    timeline

--- ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/control/CustomerJourneyTimelineChannel.js
+++ ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/control/CustomerJourneyTimelineChannel.js
@@ -2,11 +2,12 @@
 
 sap.ui.define(
     [
+        'jquery.sap.global',
         'sap/ui/core/Control',
         'test/tm/ze_ccux_ctrl/view/control/CustomerJourneyTimelineChannelRenderer'
     ],
 
-    function (Control, CustomRenderer) {
+    function (jQuery, Control, CustomRenderer) {
         'use strict';
 
         var CustomControl = Control.extend('test.tm.ze_ccux_ctrl.view.control.CustomerJourneyTimelineChannel', {
@@ -39,12 +40,19 @@ sap.ui.define(
         };
 
         CustomControl.prototype.onclick = function (oEvent) {
-            console.log('onclick');
-            this.firePress();
+            this._onDoubleClick = false;
+
+            // Wait for a while to check whether user intention is to double click.
+            //  If it is, do not fire single click.
+            jQuery.sap.delayedCall(300, this, function () {
+                if (!this._onDoubleClick) {
+                    this.firePress();
+                }
+            });
         };
 
         CustomControl.prototype.ondblclick = function (oEvent) {
-            console.log('ondblclick');
+            this._onDoubleClick = true;
             this.fireDoublePress();
         };
 
