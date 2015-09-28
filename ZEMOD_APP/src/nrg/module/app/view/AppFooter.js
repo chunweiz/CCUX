73fe6cb83721686194aeef4677658255ee777b63
jerryhsu <jerry.hsu@utegration.com>
2015-09-28 14:53:50 -0500

    footer

--- ZEMOD_APP/src/nrg/module/app/view/AppFooter.js
+++ ZEMOD_APP/src/nrg/module/app/view/AppFooter.js
@@ -40,32 +40,32 @@ sap.ui.define(
         };
 
         AppFooter.prototype.updateFooter = function (oPayload) {
-            var bp = '';
-            var ca = '';
-            var co = '32253375';
-            var oFilterTemplate = new Filter({ path: 'Contract', operator: FilterOperator.EQ, value1: co});
-            var sPath = '/CpgFtrS';
-            var aFilters = [];
-                aFilters.push(oFilterTemplate);
-            var oModel = this._oController.getView().getModel('oODataSvc'),
-                oParameters;
+            // var bp = '';
+            // var ca = '';
+            // var co = '32253375';
+            // var oFilterTemplate = new Filter({ path: 'Contract', operator: FilterOperator.EQ, value1: co});
+            // var sPath = '/CpgFtrS';
+            // var aFilters = [];
+            //     aFilters.push(oFilterTemplate);
+            // var oModel = this._oController.getView().getModel('oODataSvc'),
+            //     oParameters;
 
-            oParameters = {
-                filters: aFilters,
-                success : function (oData) {
-                    if (oData) {
-                        var oModel = this._oController.getView().getModel('oFooterCampaign');
-                        oModel.setProperty('/Current', oData.results[0]);
-                    }
-                }.bind(this),
-                error: function (oError) {
-                    //Need to put error message
-                }.bind(this)
-            };
+            // oParameters = {
+            //     filters: aFilters,
+            //     success : function (oData) {
+            //         if (oData) {
+            //             var oModel = this._oController.getView().getModel('oFooterCampaign');
+            //             oModel.setProperty('/Current', oData.results[0]);
+            //         }
+            //     }.bind(this),
+            //     error: function (oError) {
+            //         //Need to put error message
+            //     }.bind(this)
+            // };
 
-            if (oModel) {
-                oModel.read(sPath, oParameters);
-            }
+            // if (oModel) {
+            //     oModel.read(sPath, oParameters);
+            // }
         };
 
         AppFooter.prototype.reset = function () {
