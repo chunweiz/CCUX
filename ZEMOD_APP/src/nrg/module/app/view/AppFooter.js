a95b61017e4b11ce8921b4f60d3d9d4cc2bbc93c
jerryhsu <jerry.hsu@utegration.com>
2015-09-28 17:07:20 -0500

    footer

--- ZEMOD_APP/src/nrg/module/app/view/AppFooter.js
+++ ZEMOD_APP/src/nrg/module/app/view/AppFooter.js
@@ -54,8 +54,8 @@ sap.ui.define(
                 filters: aFilters,
                 success : function (oData) {
                     if (oData) {
-                        // var oCampaignModel = this._oController.getView().getModel('oFooterCampaign');
-                        // oCampaignModel.setData({Current:{OfferTitle: "None"}, Pending:{OfferTitle: "None"}, History:{OfferTitle: "None"}});
+                        var oCampaignModel = this._oController.getView().getModel('oFooterCampaign');
+                        oCampaignModel.setData({Current:{OfferTitle: "None"}, Pending:{OfferTitle: "None"}, History:{OfferTitle: "None"}});
 
                         // for (var i = 0; i < oData.results.length; i++) {
                         //     if (oData.results[i].Type === 'C') {
