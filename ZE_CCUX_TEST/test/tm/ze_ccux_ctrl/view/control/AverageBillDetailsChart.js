c44661765600b30a926d586dd782fc1fd7ff6d82
tmhew <tauming.hew@utegration.com>
2015-10-05 14:17:22 -0500

    avg x axis

--- ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/control/AverageBillDetailsChart.js
+++ ZE_CCUX_TEST/test/tm/ze_ccux_ctrl/view/control/AverageBillDetailsChart.js
@@ -14,8 +14,8 @@ sap.ui.define(
             metadata: {
                 properties: {
                     width: { type: 'int', defaultValue: 600 },
-                    height: { type: 'int', defaultValue: 400 },
-                    usageTickSize: { type: 'int', defaultValue: 500 }
+                    height: { type: 'int', defaultValue: 300 },
+                    usageTickSize: { type: 'int', defaultValue: 1000 }
                 }
             },
 
@@ -63,17 +63,19 @@ sap.ui.define(
 
         CustomControl.prototype._createChart = function () {
             var oCustomControl = this;
-            var oMargin = { top: 50, right: 60, bottom: 60, left: 100 };
+            var oMargin = { top: 0, right: 60, bottom: 60, left: 100 };
             var iWidth = 600 - oMargin.left - oMargin.right;
-            var iHeight = 400 - oMargin.top - oMargin.bottom - 50;
+            var iHeight = 300 - oMargin.top - oMargin.bottom - 50;
             var aDataset = oCustomControl._getDataSet();
 
             // X scale - month
+            var aMinMaxMonth = d3.extent(aDataset, function (data) { return data.usageDate.getMonth(); });
+
             var fnScaleMonth = d3.scale.linear()
-                .domain(d3.extent(aDataset, function (data) { return data.usageDate.getMonth(); }))
+                .domain(aMinMaxMonth)
                 .range([0, iWidth]);
 
-            // Y scale - kwh usage based on usage tick size
+            // Y scale - kwh usage
             var iMaxUsage = d3.max(aDataset, function (data) { return data.usage; });
             var iUsageTickSize = oCustomControl.getUsageTickSize();
 
@@ -84,11 +86,30 @@ sap.ui.define(
             // Create a canvas with margin
             var oCanvas = d3.select('#' + this.getId())
                 .append('svg')
+                    .attr('class', 'tmAVDChart')
                     .attr('width', oCustomControl.getWidth())
                     .attr('height', oCustomControl.getHeight())
                     .attr('viewBox', [0, 0, iWidth + oMargin.left + oMargin.right, iHeight + oMargin.top + oMargin.bottom].join(' '))
                     .append('g')
                         .attr('transform', 'translate(' + [ oMargin.left, oMargin.top ] + ')');
+
+            // X axis - month
+            var fnXAxisLabel = d3.scale.ordinal()
+                .domain(d3.range(12))
+                .range(['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']);
+
+            var fnAxisMonth = d3.svg.axis()
+                .scale(fnScaleMonth)
+                .orient('bottom')
+                .tickValues(d3.range(aMinMaxMonth[0], aMinMaxMonth[1] + 1))
+                .tickFormat(function (data) { return fnXAxisLabel(data); });
+
+            oCanvas.append('g')
+                .attr('class', 'tmAVDChart-XAxis')
+                .attr('transform', 'translate(' + [0, iHeight] + ')')
+                .call(fnAxisMonth);
+
+            // Y axis - kwh usage based on usage tick size
 
             // Average usage lines
             var aDatasetByYear = d3.nest()
@@ -101,26 +122,37 @@ sap.ui.define(
 
             var fnUsageLine = d3.svg.line()
                 .x(function (data) { return fnScaleMonth(data.usageDate.getMonth()); })
-                .y(function (data) { return fnScaleUsage(data.usage); });
+                .y(function (data) { return fnScaleUsage(data.usage); })
+                .interpolate('cardinal');
 
             var oUsageLines = oCanvas.append('g').selectAll('g')
                 .data(aDatasetByYear)
                 .enter().append('g')
-                    .append('path')
-                        .attr('class', 'tmAVDChart-usageLine')
-                        .attr('d', function (data) { return fnUsageLine(data.values); })
-                        .style('stroke', function (data) { return fnLineColor(data.key); })
-                        .style('fill', 'none');
+                    .attr('class', 'tmAVDChart-usage');
+
+            oUsageLines.append('path')
+                .attr('class', 'tmAVDChart-usageLine')
+                .attr('d', function (data) { return fnUsageLine(data.values); })
+                .style('stroke', function (data) { return fnLineColor(data.key); })
+                .style('fill', 'none');
 
             // Average usage data points
-            var oUsageDataPoints = oCanvas.append('g').selectAll('circle')
-                .data(aDataset)
-                .enter().append('circle')
+            oUsageLines.selectAll('g')
+                .data(function (data) { return data.values; })
+                .enter()
+                .append('circle')
                     .attr('class', 'tmAVDChart-usageDataPoint')
                     .attr('r', '3')
                     .attr('cx', function (data) { return fnScaleMonth(data.usageDate.getMonth()); })
                     .attr('cy', function (data) { return fnScaleUsage(data.usage); })
                     .style('fill', function (data) { return fnLineColor(data.usageDate.getFullYear()); });
+
+            // Selectively display usage lines
+            // oCanvas.selectAll('.tmAVDChart-usage').each(function (oData) {
+            //     if (oData.key !== '2015') {
+            //         d3.select(this).style('display', 'none');
+            //     }
+            // });
 
         };
 
