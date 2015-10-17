fb578f9c7860cf639fc1d910617284c9a695d8b7
jerryhsu <jerry.hsu@utegration.com>
2015-10-17 15:44:08 -0500

    ABP

--- ZEMOD_BILLING/src/nrg/module/billing/view/BillingCheckbookTools.controller.js
+++ ZEMOD_BILLING/src/nrg/module/billing/view/BillingCheckbookTools.controller.js
@@ -134,6 +134,13 @@ sap.ui.define(
             oParameters = {
                 success : function (oData) {
                     oEligibilityModel.setData(oData);
+                    if (oData.ABPAct === "Y") {
+                        oEligibilityModel.setProperty('/Activated', true);
+                        oEligibilityModel.setProperty('/NonActivated', false);
+                    } else {
+                        oEligibilityModel.setProperty('/Activated', false);
+                        oEligibilityModel.setProperty('/NonActivated', true);
+                    }
                 }.bind(this),
                 error: function (oError) {
                 
@@ -168,62 +175,49 @@ sap.ui.define(
                 oPayload[adjustParameterName] = oHistoryModel.oData[i].AdjAmount;
             }
 
-            // if (oModel) {
-            //     oModel.callFunction(
-            //         '/ABPCalc',     // function name
-            //         'POST',         // http method
-            //         oPayload,       // parameters
-            //         function (oData, response) {    // callback function for success  
-            //             console.log('5566520', oData, response);
-            //         },
-            //         function (oError) {             // callback function for error  
-            //             console.log('7788520', oError);
-            //         }
-            //     );
-            // }
-
             if (oModel) {
                 mParameters = {
                     method : "POST",
-                    urlParameters : {
-                        "Contract": this._coNum,
-                        "Prd01": oPayload.Prd01,
-                        "Prd02": oPayload.Prd02,
-                        "Prd03": oPayload.Prd03,
-                        "Prd04": oPayload.Prd04,
-                        "Prd05": oPayload.Prd05,
-                        "Prd06": oPayload.Prd06,
-                        "Prd07": oPayload.Prd07,
-                        "Prd08": oPayload.Prd08,
-                        "Prd09": oPayload.Prd09,
-                        "Prd10": oPayload.Prd10,
-                        "Prd11": oPayload.Prd11,
-                        "Prd12": oPayload.Prd12,
-                        "Amt01": parseFloat(oPayload.Amt01) || 0,
-                        "Amt02": parseFloat(oPayload.Amt02) || 0,
-                        "Amt03": parseFloat(oPayload.Amt03) || 0,
-                        "Amt04": parseFloat(oPayload.Amt04) || 0,
-                        "Amt05": parseFloat(oPayload.Amt05) || 0,
-                        "Amt06": parseFloat(oPayload.Amt06) || 0,
-                        "Amt07": parseFloat(oPayload.Amt07) || 0,
-                        "Amt08": parseFloat(oPayload.Amt08) || 0,
-                        "Amt09": parseFloat(oPayload.Amt09) || 0,
-                        "Amt10": parseFloat(oPayload.Amt10) || 0,
-                        "Amt11": parseFloat(oPayload.Amt11) || 0,
-                        "Amt12": parseFloat(oPayload.Amt12) || 0,
-                        "Bbs01": parseFloat(oPayload.Bbs01) || 0,
-                        "Bbs02": parseFloat(oPayload.Bbs02) || 0,
-                        "Bbs03": parseFloat(oPayload.Bbs03) || 0,
-                        "Bbs04": parseFloat(oPayload.Bbs04) || 0,
-                        "Bbs05": parseFloat(oPayload.Bbs05) || 0,
-                        "Bbs06": parseFloat(oPayload.Bbs06) || 0,
-                        "Bbs07": parseFloat(oPayload.Bbs07) || 0,
-                        "Bbs08": parseFloat(oPayload.Bbs08) || 0,
-                        "Bbs09": parseFloat(oPayload.Bbs09) || 0,
-                        "Bbs10": parseFloat(oPayload.Bbs10) || 0,
-                        "Bbs11": parseFloat(oPayload.Bbs11) || 0,
-                        "Bbs12": parseFloat(oPayload.Bbs12) || 0
-                    },
+                    // urlParameters : {
+                    //     "Contract": this._coNum,
+                    //     "Prd01": oPayload.Prd01,
+                    //     "Prd02": oPayload.Prd02,
+                    //     "Prd03": oPayload.Prd03,
+                    //     "Prd04": oPayload.Prd04,
+                    //     "Prd05": oPayload.Prd05,
+                    //     "Prd06": oPayload.Prd06,
+                    //     "Prd07": oPayload.Prd07,
+                    //     "Prd08": oPayload.Prd08,
+                    //     "Prd09": oPayload.Prd09,
+                    //     "Prd10": oPayload.Prd10,
+                    //     "Prd11": oPayload.Prd11,
+                    //     "Prd12": oPayload.Prd12,
+                    //     "Amt01": parseFloat(oPayload.Amt01) || 0,
+                    //     "Amt02": parseFloat(oPayload.Amt02) || 0,
+                    //     "Amt03": parseFloat(oPayload.Amt03) || 0,
+                    //     "Amt04": parseFloat(oPayload.Amt04) || 0,
+                    //     "Amt05": parseFloat(oPayload.Amt05) || 0,
+                    //     "Amt06": parseFloat(oPayload.Amt06) || 0,
+                    //     "Amt07": parseFloat(oPayload.Amt07) || 0,
+                    //     "Amt08": parseFloat(oPayload.Amt08) || 0,
+                    //     "Amt09": parseFloat(oPayload.Amt09) || 0,
+                    //     "Amt10": parseFloat(oPayload.Amt10) || 0,
+                    //     "Amt11": parseFloat(oPayload.Amt11) || 0,
+                    //     "Amt12": parseFloat(oPayload.Amt12) || 0,
+                    //     "Bbs01": parseFloat(oPayload.Bbs01) || 0,
+                    //     "Bbs02": parseFloat(oPayload.Bbs02) || 0,
+                    //     "Bbs03": parseFloat(oPayload.Bbs03) || 0,
+                    //     "Bbs04": parseFloat(oPayload.Bbs04) || 0,
+                    //     "Bbs05": parseFloat(oPayload.Bbs05) || 0,
+                    //     "Bbs06": parseFloat(oPayload.Bbs06) || 0,
+                    //     "Bbs07": parseFloat(oPayload.Bbs07) || 0,
+                    //     "Bbs08": parseFloat(oPayload.Bbs08) || 0,
+                    //     "Bbs09": parseFloat(oPayload.Bbs09) || 0,
+                    //     "Bbs10": parseFloat(oPayload.Bbs10) || 0,
+                    //     "Bbs11": parseFloat(oPayload.Bbs11) || 0,
+                    //     "Bbs12": parseFloat(oPayload.Bbs12) || 0
+                    // },
+                    urlParameters : oPayload,
                     success : function (oData, response) {
                         console.log('5566520', oData, response);
                     }.bind(this),
@@ -233,7 +227,51 @@ sap.ui.define(
                 };
                 oModel.callFunction("/ABPCalc", mParameters);
             }
+        };
 
+        Controller.prototype._onSetBtnClick = function () {
+            var oModel = this.getView().getModel('oDataAvgSvc'),
+                oHistoryModel = this.getView().getModel('oAmountHistory'),
+                mParameters;
+
+            if (oModel) {
+                mParameters = {
+                    method : "POST",
+                    urlParameters : {
+                        "Contract": this._coNum,
+                        "Date": oHistoryModel.oData[oHistoryModel.oData.length - 1].Period
+                    },
+                    success : function (oData, response) {
+                        console.log('5566520', oData, response);
+                    }.bind(this),
+                    error: function (oError) {
+                        console.log('7788520', oError);
+                    }.bind(this)
+                };
+                oModel.callFunction("/ABPCrte", mParameters);
+            }
+        };
+
+        Controller.prototype._onDeactBtnClick = function () {
+            var oModel = this.getView().getModel('oDataAvgSvc'),
+                oHistoryModel = this.getView().getModel('oAmountHistory'),
+                mParameters;
+
+            if (oModel) {
+                mParameters = {
+                    method : "POST",
+                    urlParameters : {
+                        "Contract": this._coNum
+                    },
+                    success : function (oData, response) {
+                        console.log('5566520', oData, response);
+                    }.bind(this),
+                    error: function (oError) {
+                        console.log('7788520', oError);
+                    }.bind(this)
+                };
+                oModel.callFunction("/ABPCanc", mParameters);
+            }
         };
 
         Controller.prototype._onAvgBillBtnClicked = function () {
