8991d8f65d69e1220fc859a466fb74de69b32a2d
unknown <hlin@TXHQHLIN-L1.nrgenergy.com>
2015-10-19 16:26:29 -0500

    working on prepay

--- ZEMOD_BILLING/src/nrg/module/billing/view/PrePayCheckbook.controller.js
+++ ZEMOD_BILLING/src/nrg/module/billing/view/PrePayCheckbook.controller.js
@@ -1,3 +1,6 @@
+/*globals sap*/
+/*jslint nomen:true*/
+
 sap.ui.define(
     [
         'jquery.sap.global',
@@ -19,9 +22,124 @@ sap.ui.define(
 
             //Model to keep checkbook header
             this.getView().setModel(new sap.ui.model.json.JSONModel(), 'oPpChkbkHdr');
-            this.getView().setModel(new sap.ui.model.json.JSONModel(), 'oPpPaymentHdr');
+            this.getView().setModel(new sap.ui.model.json.JSONModel(), 'oPpPmtHdr');
+
+            this._initRoutingInfo();
+            this._initPpChkbookHdr();
+            this._initPpPmtHdr();
+        };
+
+        /**********************************************************************************************************************************************************/
+        //Formatters
+        /**********************************************************************************************************************************************************/
+        CustomController.prototype._formatDate = function (oDate) {
+            var sFormattedDate;
+
+            if (!oDate) {
+                return null;
+            } else {
+                sFormattedDate = (oDate.getMonth() + 1).toString() + '/' + oDate.getDate().toString() + '/' + oDate.getFullYear().toString().substring(2, 4);
+                return sFormattedDate;
+            }
+        };
+
+        CustomController.prototype._formatBoolCredit = function (sAccBal) {
+            if (parseInt(sAccBal, 10) >= 0) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+        CustomController.prototype._formatBoolDebit = function (sAccBal) {
+            if (parseInt(sAccBal, 10) < 0) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+
+
+        /**********************************************************************************************************************************************************/
+        //Handlers
+        /**********************************************************************************************************************************************************/
+
+
+
+
+        /**********************************************************************************************************************************************************/
+        //Init functions
+        /**********************************************************************************************************************************************************/
+        CustomController.prototype._initRoutingInfo = function () {
+            var oRouteInfo = this.getOwnerComponent().getCcuxRouteManager().getCurrentRouteInfo();
+
+            this._bpNum = oRouteInfo.parameters.bpNum;
+            this._caNum = oRouteInfo.parameters.caNum;
+            this._coNum = oRouteInfo.parameters.coNum;
+        };
+
+        CustomController.prototype._initPpChkbookHdr = function () {
+            var sPath;
+
+            sPath = '/PrePayHeaders(\'' + this._caNum + '\')';
+            this._retrPpChkbookHdr(sPath);
+        };
+
+        CustomController.prototype._initPpPmtHdr = function () {
+            var sPath;
+
+            //sPath = '/ConfBuags(\'' + this._caNum + '\')/PrePayPmtHdr';
+            sPath = '/PrePayPmtHdrs(ContractAccountNumber=\'' + this._caNum + '\',ActKey=\'000001\')';
+
+
+            this._retrPpPmtHdr(sPath);
+        };
+
+
+        /**********************************************************************************************************************************************************/
+        //OData Communication Functions
+        /**********************************************************************************************************************************************************/
+        CustomController.prototype._retrPpChkbookHdr = function (sPath) {
+            var oChbkOData = this.getView().getModel('oDataSvc'),
+                oParameters;
+
+            oParameters = {
+                success : function (oData) {
+                    if (oData) {
+                        this.getView().getModel('oPpChkbkHdr').setData(oData);
+                    }
+                }.bind(this),
+                error: function (oError) {
+                    //Need to put error message
+                }.bind(this)
+            };
+
+            if (oChbkOData) {
+                oChbkOData.read(sPath, oParameters);
+            }
+        };
+
+        CustomController.prototype._retrPpPmtHdr = function (sPath) {
+            var oChbkOData = this.getView().getModel('oDataSvc'),
+                oParameters;
+
+            oParameters = {
+                success : function (oData) {
+                    if (oData) {
+                        this.getView().getModel('oPpPmtHdr').setData(oData);
+                    }
+                }.bind(this),
+                error: function (oError) {
+                    //Need to put error message
+                }.bind(this)
+            };
+
+            if (oChbkOData) {
+                oChbkOData.read(sPath, oParameters);
+            }
 
         };
+
 
         return CustomController;
     }
