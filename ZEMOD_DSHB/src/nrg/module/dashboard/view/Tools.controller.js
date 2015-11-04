e7a00ed48b9af97f4eaaf1909c807035cf36597c
unknown <hlin@TXHQHLIN-L1.nrgenergy.com>
2015-11-04 17:30:50 -0600

    service order pending done // add reconnection page

--- ZEMOD_DSHB/src/nrg/module/dashboard/view/Tools.controller.js
+++ ZEMOD_DSHB/src/nrg/module/dashboard/view/Tools.controller.js
@@ -12,6 +12,16 @@ sap.ui.define(
         var Controller = CoreController.extend('nrg.module.dashboard.view.Tools');
 
         //TODO: Implementation required
+        Controller.prototype._onReconnectionClick = function () {
+            if (!this._oReconnectPopup) {
+                this._oReconnectPopup = ute.ui.main.Popup.create({
+				    content: sap.ui.xmlfragment(this.getView().sId, "nrg.module.dashboard.view.Reconnect", this),
+					title: 'RECONNETION'
+				});
+				this.getView().addDependent(this._oReconnectPopup);
+            }
+            this._oReconnectPopup.open();
+        };
 
         return Controller;
     }
