2b9b256855868c1509d25be4f2b9bebef13f5df6
unknown <hlin@TXHQHLIN-L1.nrgenergy.com>
2015-11-11 16:15:53 -0600

    reconnect

--- ZEMOD_DSHB/src/nrg/module/dashboard/view/Tools.controller.js
+++ ZEMOD_DSHB/src/nrg/module/dashboard/view/Tools.controller.js
@@ -1,17 +1,44 @@
 /*globals sap*/
+/*globals ute*/
 /*jslint nomen:true*/
 
 sap.ui.define(
     [
-        'sap/ui/core/mvc/Controller'
+        'nrg/base/view/BaseController',
+        'sap/ui/model/Filter',
+        'sap/ui/model/FilterOperator'
     ],
 
-    function (CoreController) {
+    function (CoreController, Filter, FilterOperator) {
         'use strict';
 
         var Controller = CoreController.extend('nrg.module.dashboard.view.Tools');
 
-        //TODO: Implementation required
+        /**********************************************************************************************************************/
+        //On Start
+        /**********************************************************************************************************************/
+        Controller.prototype.onInit = function () {
+        };
+
+        Controller.prototype.onBeforeRendering = function () {
+            var oRouteInfo = this.getOwnerComponent().getCcuxRouteManager().getCurrentRouteInfo();
+
+            this._bpNum = oRouteInfo.parameters.bpNum;
+            this._caNum = oRouteInfo.parameters.caNum;
+            this._coNum = oRouteInfo.parameters.coNum;
+
+            this.getView().setModel(this.getOwnerComponent().getModel('comp-dashboard-svcodr'), 'oODataSvc');
+
+            //Model to keep Reconnect info and status
+            this.getView().setModel(new sap.ui.model.json.JSONModel(), 'oReconnectInfo');
+
+
+        };
+
+
+        /**********************************************************************************************************************/
+        //Handlers
+        /**********************************************************************************************************************/
         Controller.prototype._onReconnectionClick = function () {
             if (!this._oReconnectPopup) {
                 this._oReconnectPopup = ute.ui.main.Popup.create({
@@ -22,6 +49,48 @@ sap.ui.define(
 				this.getView().addDependent(this._oReconnectPopup);
             }
             this._oReconnectPopup.open();
+            this._retrReconnectInfo();
+
+        };
+
+        Controller.prototype._onReconnectClicked = function () {
+
+        };
+
+        Controller.prototype._onReconnectCancelClicked = function () {
+            this._oReconnectPopup.close();
+        };
+
+
+        /**********************************************************************************************************************/
+        //Request Functions
+        /**********************************************************************************************************************/
+        Controller.prototype._retrReconnectInfo = function () {
+            var sPath,
+                aFilters = [],
+                oParameters,
+                oModel = this.getView().getModel('oODataSvc');
+
+
+            aFilters.push(new Filter({ path: 'PartnerID', operator: FilterOperator.EQ, value1: this._bpNum}));
+            aFilters.push(new Filter({ path: 'BuagID', operator: FilterOperator.EQ, value1: this._caNum}));
+
+            sPath = '/Reconnects';
+
+            oParameters = {
+                filters: aFilters,
+                success : function (oData) {
+                    if (oData) {
+                        this.getView().getModel('oReconnectInfo').setData(oData);
+                    }
+                }.bind(this),
+                error: function (oError) {
+                }.bind(this)
+            };
+
+            if (oModel) {
+                oModel.read(sPath, oParameters);
+            }
         };
 
         return Controller;
