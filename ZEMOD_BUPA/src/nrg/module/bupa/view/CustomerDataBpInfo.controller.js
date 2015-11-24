f6fa8a0b8067815896fba1f0ad7c87b1276af8ae
jerryhsu <jerry.hsu@utegration.com>
2015-11-24 14:11:22 -0600

    fix defect #46

--- ZEMOD_BUPA/src/nrg/module/bupa/view/CustomerDataBpInfo.controller.js
+++ ZEMOD_BUPA/src/nrg/module/bupa/view/CustomerDataBpInfo.controller.js
@@ -753,13 +753,13 @@ sap.ui.define(
                 success : function (oData) {
                     if (oData) {
                         if (oData.PartnerID) {
+                            // Make the key of empty value as "0000", so that the dropdown can recognize it.
+                            if (oData.Title === "") oData.Title = "0000";
+                            if (oData.AcademicTitle === "") oData.AcademicTitle = "0000";
+                            if (oData.Suffix === "") oData.Suffix = "0000";
+                            
                             this.getView().getModel('ODataBpName').setData(oData);
                             this.ODataBpNameBak = jQuery.extend(true, {}, oData);
-
-                            // Make the key of empty value as "0000", so that the dropdown can recognize it.
-                            if (this.ODataBpNameBak.Title === "") this.ODataBpNameBak.Title = "0000";
-                            if (this.ODataBpNameBak.AcademicTitle === "") this.ODataBpNameBak.AcademicTitle = "0000";
-                            if (this.ODataBpNameBak.Suffix === "") this.ODataBpNameBak.Suffix = "0000";
                         }
                     }
                 }.bind(this),
