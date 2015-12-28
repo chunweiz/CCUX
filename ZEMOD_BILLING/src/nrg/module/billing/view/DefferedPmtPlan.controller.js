156637545c5060a9bb30ab9370c18887a12c8cc7
hjlin <hlin@TXHQHLIN-L1.nrgenergy.com>
2015-12-28 17:27:19 -0600

    DPP

--- ZEMOD_BILLING/src/nrg/module/billing/view/DefferedPmtPlan.controller.js
+++ ZEMOD_BILLING/src/nrg/module/billing/view/DefferedPmtPlan.controller.js
@@ -608,7 +608,7 @@ sap.ui.define(
             sPath = '/DPPConfs';
 
             oParameters = {
-                //filters: aFilters,
+                filters: aFilters,
                 success : function (oData) {
                     if (oData) {
                         this.getView().getModel('oDppConfs').setData(oData);
