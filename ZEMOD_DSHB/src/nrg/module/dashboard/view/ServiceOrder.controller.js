c2795a54900a9e84a92a698911bcc6774e9b6712
jerryhsu <jerry.hsu@utegration.com>
2015-12-30 17:18:38 -0600

    fixing defect #252

--- ZEMOD_DSHB/src/nrg/module/dashboard/view/ServiceOrder.controller.js
+++ ZEMOD_DSHB/src/nrg/module/dashboard/view/ServiceOrder.controller.js
@@ -168,7 +168,7 @@ sap.ui.define(
                         for (i = 0; i < oData.results.length; i = i + 1) {
                             oData.results[i].iInd = i;
                             // Select the CO passed from dashboard
-                            if (oData.results[i].Contract.trim() === this._coNum) {
+                            if (oData.results[i].Contract.trim() === this._coNum.trim()) {
                                 this.getView().byId('idESIDDropdown').setSelectedKey(oData.results[i].iInd);
                             }
                         }
