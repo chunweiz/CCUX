2e56b52a1a1ae776d787daa57a7a3758c7a7d245
SarathChandrach <sarath.chandra@nrg.com>
2015-12-30 17:27:31 -0600

    Fee Adjustments

--- ZEMOD_BILLING/src/nrg/module/billing/view/FeeAdjustments.controller.js
+++ ZEMOD_BILLING/src/nrg/module/billing/view/FeeAdjustments.controller.js
@@ -18,19 +18,99 @@ sap.ui.define(
 		/* =========================================================== */
         Controller.prototype.onBeforeRendering = function () {
             var oViewModel = new JSONModel({
-                    discNoticefee : false,  // true for invoice & false for consumption
-                    discRecovfee : true,  // true for first Card change, false for second card change for Invoice
-                    Latefee : true, // true for first Card change, false for second card change for Consumption
+                    discNoticefee : false,  // Disc Notice Fee is pre-selected
+                    discRecovfee : true,
+                    Latefee : true,
                     Reconnectfee : true,
                     feeDateDD : true,
                     reasonDD: false,
                     ok: false
 			    }),
-                oRouteInfo = this.getOwnerComponent().getCcuxRouteManager().getCurrentRouteInfo();
+                oRouteInfo = this.getOwnerComponent().getCcuxRouteManager().getCurrentRouteInfo(),
+                oCADropDown = this.getView().byId("idnrgFeeAdj-DropDownCA"),
+                oCATemplate = this.getView().byId("idnrgFeeAdj-DropDownCA-temp"),
+                oDisconnectDropDown = this.getView().byId("idnrgFeeAdj-DropDownDate"),
+                oDisconnectTemplate = this.getView().byId("idnrgFeeAdj-DropDownDate-temp"),
+                aFilterIds,
+                aFilterValues,
+                aFilters,
+                sPath,
+                oBindingInfo,
+                fnRecievedHandler,
+                that = this;
             this._sContract = oRouteInfo.parameters.coNum;
             this._sBP = oRouteInfo.parameters.bpNum;
             this._sCA = oRouteInfo.parameters.caNum;
-            this.getView().setModel(oViewModel, "comp-feeAdj");
+            this.getView().setModel(oViewModel, "view-feeAdj");
+            aFilterIds = ["BP"];
+            aFilterValues = ['2473499'];
+            aFilters = this._createSearchFilterObject(aFilterIds, aFilterValues);
+            sPath = "/ContractAcctS";
+            fnRecievedHandler = function (oEvent, oData) {
+                var aContent = oCADropDown.getContent();
+                if ((aContent) && (aContent.length > 0)) {
+                    oCADropDown.setSelectedKey(aContent[0].getKey());
+                    aFilterIds = ["CA"];
+                    aFilterValues = ['000004014634'];
+                    aFilters = that._createSearchFilterObject(aFilterIds, aFilterValues);
+                    sPath = "/DiscNoticeFeeS";
+                    oBindingInfo = {
+                        model : "comp-feeAdjs",
+                        path : sPath,
+                        template : oDisconnectTemplate,
+                        filters : aFilters
+                    };
+                    oDisconnectDropDown.bindAggregation("content", oBindingInfo);
+                }
+            };
+            oBindingInfo = {
+                model : "comp-feeAdjs",
+                path : sPath,
+                template : oCATemplate,
+                filters : aFilters,
+                events: {dataReceived : fnRecievedHandler}
+            };
+            oCADropDown.bindAggregation("content", oBindingInfo);
+        };
+       /**
+		 * Assign the filter objects based on the input selection
+		 *
+		 * @function
+		 * @param {Array} aFilterIds to be used as sPath for Filters
+         * @param {Array} aFilterValues for each sPath
+		 * @private
+		 */
+        Controller.prototype._createSearchFilterObject = function (aFilterIds, aFilterValues) {
+            var aFilters = [],
+                iCount;
+
+            for (iCount = 0; iCount < aFilterIds.length; iCount = iCount + 1) {
+                aFilters.push(new Filter(aFilterIds[iCount], FilterOperator.EQ, aFilterValues[iCount], ""));
+            }
+            return aFilters;
+        };
+        /**
+		 * Clicked on Submit
+		 *
+		 * @function
+         * @param {sap.ui.base.Event} oEvent pattern match event
+		 */
+        Controller.prototype.onSubmit = function (oEvent) {
+            var oModel = this.getOwnerComponent().getModel('comp-feeAdjs'),
+                mParameters = {
+                    method : "POST",
+                    urlParameters : {"Amount" : "",
+                                             "CA" : "",
+                                            "DocNum" : "",
+                                            "Reason" : "",
+                                            "Text" : ""},
+                    success : function (oData) {
+                    }.bind(this),
+                    error: function (oError) {
+
+                    }.bind(this)
+                };
+            oModel.callFunction("/RemoveFee", mParameters); // callback function for error
         };
         /**
 		 * Clicked on Disconnect Notice Fee
@@ -39,6 +119,11 @@ sap.ui.define(
          * @param {sap.ui.base.Event} oEvent pattern match event
 		 */
         Controller.prototype.onDiscNoticeFee = function (oEvent) {
+            var oViewModel = this.getView().getModel("view-feeAdj");
+            oViewModel.setProperty("/discNoticefee", false);
+            oViewModel.setProperty("/discRecovfee", true);
+            oViewModel.setProperty("/Latefee", true);
+            oViewModel.setProperty("/Reconnectfee", true);
         };
         /**
 		 * Clicked on Disconnect Recovery Fee
@@ -47,6 +132,11 @@ sap.ui.define(
          * @param {sap.ui.base.Event} oEvent pattern match event
 		 */
         Controller.prototype.onDiscRecovFee = function (oEvent) {
+            var oViewModel = this.getView().getModel("view-feeAdj");
+            oViewModel.setProperty("/discNoticefee", true);
+            oViewModel.setProperty("/discRecovfee", false);
+            oViewModel.setProperty("/Latefee", true);
+            oViewModel.setProperty("/Reconnectfee", true);
         };
         /**
 		 * Clicked on Late Fee
@@ -55,6 +145,11 @@ sap.ui.define(
          * @param {sap.ui.base.Event} oEvent pattern match event
 		 */
         Controller.prototype.onLateFee = function (oEvent) {
+            var oViewModel = this.getView().getModel("view-feeAdj");
+            oViewModel.setProperty("/discNoticefee", true);
+            oViewModel.setProperty("/discRecovfee", true);
+            oViewModel.setProperty("/Latefee", false);
+            oViewModel.setProperty("/Reconnectfee", true);
         };
         /**
 		 * Clicked on Reconnect Fee
@@ -63,6 +158,11 @@ sap.ui.define(
          * @param {sap.ui.base.Event} oEvent pattern match event
 		 */
         Controller.prototype.onReconnectFee = function (oEvent) {
+            var oViewModel = this.getView().getModel("view-feeAdj");
+            oViewModel.setProperty("/discNoticefee", true);
+            oViewModel.setProperty("/discRecovfee", true);
+            oViewModel.setProperty("/Latefee", true);
+            oViewModel.setProperty("/Reconnectfee", false);
         };
         return Controller;
     }
